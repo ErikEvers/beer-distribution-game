4a70d59a077b44e8ae7b295fda05d064f17421b4
Erik Evers <erikevers1996@gmail.com>
2018-11-20 17:10:15 +0100

    Changed main class in pom. Deleted folders.

--- bootstrap/src/main/java/main/Main.java
+++ bootstrap/src/main/java/main/Main.java
@@ -5,8 +5,6 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Wow dit is een main ofzo");
-        GameConfig config = new GameConfig();
-        config.test();
         TimeUnit.MINUTES.sleep(1);
     }
 }
--- gameconfiguration/src/main/java/main/GameConfig.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class GameConfig {
-    public void test () {
-        System.out.println("GameConfig println. Helemaal cooooool");
-    }
-}
