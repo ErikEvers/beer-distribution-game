40654c00846db833ae1dc6049d8cd71f4e183942
Erik Evers <erikevers1996@gmail.com>
2018-12-04 11:42:30 +0100

    ..

--- bootstrap/src/test/java/org/han/asd/c/TestMain.java
+++ bootstrap/src/test/java/org/han/asd/c/TestMain.java
@@ -3,8 +3,15 @@ package org.han.asd.c;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestMain {
+
+    @Test
+    public void testMain(){
+        Main.main(new String[0]);
+        assertTrue(true);
+    }
     @Test
     public void testGetValueReturnsFalse(){
         Main main = new Main();
