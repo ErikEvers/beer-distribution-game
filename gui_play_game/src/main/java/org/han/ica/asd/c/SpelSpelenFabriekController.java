9a61ecf35b0bcff980ecd45b6c05db0eab3e5ed4
Chico Gerrits <CAGG.Gerrits@student.han.nl>
2018-12-04 15:56:28 +0100

    fixing other branch

--- gui_play_game/src/main/java/org/han/ica/asd/c/SpelSpelenFabriekController.java
+++ gui_play_game/src/main/java/org/han/ica/asd/c/SpelSpelenFabriekController.java
@@ -1,6 +1,7 @@
 package org.han.ica.asd.c;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
@@ -11,9 +12,18 @@ public class SpelSpelenFabriekController {
     @FXML
     AnchorPane mainContainer;
 
+    @FXML
+    TextField incomingOrderTextField;
+
+    @FXML
+    TextField step1TextField;
+
+    @FXML
+    TextField step2TextField;
+
     public void initialize(){
         mainContainer.getChildren().addAll();
         playGridPane.setStyle("-fx-border-style: solid inside;" + "-fx-border-color: black;" + "-fx-border-radius: 40;");
-
+        incomingOrderTextField.setText("50");
     }
 }
