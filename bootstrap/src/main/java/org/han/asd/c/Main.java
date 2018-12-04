15f078e929b3f14830d23f675459110577fffd4b
Erik Evers <erikevers1996@gmail.com>
2018-12-04 14:48:06 +0100

    Removed unnecessary things

--- bootstrap/src/main/java/org/han/asd/c/Main.java
+++ bootstrap/src/main/java/org/han/asd/c/Main.java
@@ -3,13 +3,5 @@ package org.han.asd.c;
 public class Main {
     public static void main(String[] args) {
         // Start the app from here
-        Main main = new Main();
-        main.getValue();
-    }
-
-    public boolean getValue(){
-        boolean value = true;
-        value = !value;
-        return value;
     }
 }
