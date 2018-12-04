77e062cbf76ccd138fceee002eed58ec88993164
Erik Evers <erikevers1996@gmail.com>
2018-12-04 10:58:41 +0100

    Added simple test for main method to check sonar quality gate for code coverage

---
+++ bootstrap/src/test/java/org/han/asd/c/TestMain.java
@@ -0,0 +1,13 @@
+package org.han.asd.c;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMain {
+    @Test
+    public void testMain(){
+        Main.main(new String[0]);
+        assertTrue(true);
+    }
+}
