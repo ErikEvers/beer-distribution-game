9e76774efeb83992723254aecfbea8530ffc1216
Bram <BM.Janssen1@student.han.nl>
2018-12-06 14:44:31 +0100

    Revert "Merge pull request #40 in SIXGJM/beer-distribution-game from feature/SIXGJM-314-integreren-prototype-gedeelte to team4"
    This reverts commit 1b07e8d0d144700cb0f4502a7a59dd1103422d42, reversing
    changes made to ad0bff16410951b644b1cb5780f17cb7b0b62229.

--- businessrule/src/main/java/org/han/ica/asd/c/businessrule/ast/Condition.java
+++
@@ -1,4 +0,0 @@
-package org.han.ica.asd.c.businessrule.ast;
-
-public abstract class Condition extends ASTNode {
-}
--- businessrule/src/main/java/org/han/ica/asd/c/businessrule/ast/comparison/Expression.java
+++
@@ -1,6 +0,0 @@
-package org.han.ica.asd.c.businessrule.ast.comparison;
-
-import org.han.ica.asd.c.businessrule.ast.Condition;
-
-abstract class Expression extends Condition {
-}
