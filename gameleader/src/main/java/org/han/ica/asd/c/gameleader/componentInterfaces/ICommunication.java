b1637a2008a8243f41a0763c129531567797658e
Yarno Boelens <yarno@print.com>
2018-12-06 12:41:19 +0100

    Added Jdocs

--- gameleader/src/main/java/org/han/ica/asd/c/gameleader/componentInterfaces/ICommunication.java
+++ gameleader/src/main/java/org/han/ica/asd/c/gameleader/componentInterfaces/ICommunication.java
@@ -1,5 +1,14 @@
 package org.han.ica.asd.c.gameleader.componentInterfaces;
 
+/**
+ * interface for communication component
+ * which is responsible for sending and receiving information, and maintaining connections throughout a game.
+ */
 public interface ICommunication {
+
+    /**
+     * The data of the game up until a specific round gets sent to the participants of said game.
+     * @param data, the game data up until that point.
+     */
     public void sendRoundData(RoundData data);
 }
