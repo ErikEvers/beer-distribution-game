8b60e73f443c3171cc111ace9605bfb54502435c
Jamiel van Engen <JL.vanEngen@student.han.nl>
2018-12-06 14:19:09 +0100

    Added the GameLogic placeOrderSendsToICommunication method to the GameLogicTest /home/jamiel/Documents/projects/school/asd/beer-distribution-game/gamelogic/src/test/java/org/han/ica/asd/c/GameLogicTes

--- gamelogic/src/test/java/org/han/ica/asd/c/GameLogicTests.java
+++ gamelogic/src/test/java/org/han/ica/asd/c/GameLogicTests.java
@@ -1,4 +1,27 @@
 package org.han.ica.asd.c;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static junit.framework.TestCase.*;
+
 
 public class GameLogicTests {
+    GameLogic gameLogic;
+
+    @Mock
+    public ICommunication communication;
+
+    @Before
+    public void setup() {
+        gameLogic = new GameLogic(communication);
+    }
+
+    @Test
+    public void placeOrderSendsToICommunication() {
+        gameLogic.placeOrder(4);
+        Mockito.verify(communication, Mockito.times(1)).send();
+    }
+
 }
