52dafe2c55dd4864fd4bfda2fd66a5bd92d84e92
Sven Timmers <S.Timmers@student.han.nl>
2018-12-07 09:18:52 +0100

    Added BusinessRuleHandler and IBusinessRules

--- businessrule/src/main/java/org/han/ica/asd/c/businessrule/BusinessRuleHandler.java
+++ businessrule/src/main/java/org/han/ica/asd/c/businessrule/BusinessRuleHandler.java
@@ -1,18 +1,26 @@
 package org.han.ica.asd.c.businessrule;
 
-
 import org.han.ica.asd.c.businessrule.parser.ParserPipeline;
 
-public class Main {
+public class BusinessRuleHandler implements IBusinessRules{
     public static void main(String[] args) {
         ParserPipeline parserPipeline = new ParserPipeline();
         parserPipeline.parseString("if inventory is 20 then order 40");
 
-//         For testing purposes
         System.out.println("---- The given input ----");
         System.out.println(parserPipeline.getBusinessRulesInput());
         System.out.println("---- The generated Businessrules Map ----");
         System.out.println(parserPipeline.getBusinessRulesMap());
     }
+
+    public void programAgent(String agentName, String businessRules){
+        ParserPipeline parserPipeline = new ParserPipeline();
+        parserPipeline.parseString("if inventory is 20 then order 40");
+        // send parsed businessrules to IBusinessRulesStore
+    }
+
+    // TODO:   public Action evaluateBusinessRules(String businessRule, RoundData roundData){
+    //        return null;
+    //    }
 
 }
