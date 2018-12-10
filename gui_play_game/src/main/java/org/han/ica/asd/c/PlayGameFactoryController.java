f6ae515eb99f0260fa8dffe03dcb73305ea8bea4
Chico Gerrits <CAGG.Gerrits@student.han.nl>
2018-12-10 09:44:28 +0100

    switch to other branch

--- gui_play_game/src/main/java/org/han/ica/asd/c/PlayGameFactoryController.java
+++ gui_play_game/src/main/java/org/han/ica/asd/c/PlayGameFactoryController.java
@@ -1,7 +1,6 @@
 package org.han.ica.asd.c;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
