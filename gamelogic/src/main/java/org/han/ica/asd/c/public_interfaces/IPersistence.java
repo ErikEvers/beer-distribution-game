a693bc4b7636e1896e52265ff1a7f5735b32d26d
Jamiel van Engen <JL.vanEngen@student.han.nl>
2018-12-11 09:54:59 +0100

    Addded the getRoundData method to IPersistence

--- gamelogic/src/main/java/org/han/ica/asd/c/public_interfaces/IPersistence.java
+++ gamelogic/src/main/java/org/han/ica/asd/c/public_interfaces/IPersistence.java
@@ -1,7 +1,11 @@
 package org.han.ica.asd.c.public_interfaces;
 
+import org.han.ica.asd.c.domain.Facility;
 import org.han.ica.asd.c.domain.Order;
+import org.han.ica.asd.c.domain.RoundData;
 
 public interface IPersistence {
     void saveOrder(Order order);
+
+    RoundData getRoundData(Facility facility);
 }
