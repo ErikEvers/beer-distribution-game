1aed9b5dfe2d60c36acdb54ad07e72cd282208b1
Erik Evers <E.Evers1@student.han.nl>
2018-12-11 15:42:37 +0100

    Changed RoundData to Round

--- businessrule/src/main/java/org/han/ica/asd/c/businessrule/BusinessRuleHandler.java
+++ businessrule/src/main/java/org/han/ica/asd/c/businessrule/BusinessRuleHandler.java
@@ -4,7 +4,7 @@ import org.han.ica.asd.c.businessrule.parser.BusinessRuleDecoder;
 import org.han.ica.asd.c.businessrule.parser.ParserPipeline;
 import org.han.ica.asd.c.businessrule.parser.ast.Action;
 import org.han.ica.asd.c.businessrule.parser.ast.BusinessRule;
-import org.han.ica.asd.c.model.RoundData;
+import org.han.ica.asd.c.model.Round;
 
 
 public class BusinessRuleHandler implements IBusinessRules{
@@ -13,7 +13,7 @@ public class BusinessRuleHandler implements IBusinessRules{
         parserPipeline.parseString(businessRules);
     }
 
-    public Action evaluateBusinessRules(String businessRules, RoundData roundData){
+    public Action evaluateBusinessRules(String businessRules, Round roundData){
         BusinessRule businessRule =  new BusinessRuleDecoder().decodeBusinessRule(businessRules);
 
         // TO-DO: 12/7/2018 Substitute variables in BusinessRule(tree)
