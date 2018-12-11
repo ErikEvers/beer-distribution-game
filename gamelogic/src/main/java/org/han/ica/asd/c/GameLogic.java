0eb56daa81d81165fb365cdd0bfcf292230397a1
Jamiel van Engen <JL.vanEngen@student.han.nl>
2018-12-11 12:20:09 +0100

    Added the throwing of the RoundDataNotFoundException to the method

--- gamelogic/src/main/java/org/han/ica/asd/c/GameLogic.java
+++ gamelogic/src/main/java/org/han/ica/asd/c/GameLogic.java
@@ -4,6 +4,7 @@ import org.han.ica.asd.c.domain.Facility;
 import org.han.ica.asd.c.domain.Order;
 import org.han.ica.asd.c.domain.PlayerRoundData;
 import org.han.ica.asd.c.domain.RoundData;
+import org.han.ica.asd.c.exceptions.RoundDataNotFoundException;
 import org.han.ica.asd.c.public_interfaces.ICommunication;
 import org.han.ica.asd.c.public_interfaces.IPersistence;
 
@@ -21,7 +22,11 @@ public class GameLogic {
         persistence.saveOrder(new Order(amount));
     }
 
-    public RoundData getRoundDataFromFacility(Facility facility) {
-        return persistence.getRoundData(facility);
+    public RoundData getRoundDataFromFacility(Facility facility) throws RoundDataNotFoundException {
+        RoundData roundData = persistence.getRoundData(facility);
+        if (roundData == null) {
+            throw new RoundDataNotFoundException();
+        }
+        return roundData;
     }
 }
