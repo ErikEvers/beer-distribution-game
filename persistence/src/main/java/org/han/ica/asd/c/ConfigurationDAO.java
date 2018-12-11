50d1bc2276dd08c2c8bca93d84d90515c49e71a2
Carsten Flokstra <C.Flokstra@student.han.nl>
2018-12-11 16:06:30 +0100

    Implemented the updateConfiguration() method

--- persistence/src/main/java/org/han/ica/asd/c/ConfigurationDAO.java
+++ persistence/src/main/java/org/han/ica/asd/c/ConfigurationDAO.java
@@ -7,7 +7,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import static org.han.ica.asd.c.dbconnection.DBConnection.RollBackTransaction;
@@ -17,10 +16,10 @@ public class ConfigurationDAO {
 	//CRUD - Create, read, update, delete
 	private static final String CREATE_CONFIGURATION = "INSERT INTO Configuration VALUES (?,?,?,?,?,?,?,?,?,?,?);";
 	private static final String READ_CONFIGURATION = "SELECT* FROM Configuration;";
-	private static final String UPDATE_CONFIGURATION = "";
+	private static final String UPDATE_CONFIGURATION = "UPDATE Configuration SET AmountOfRounds = ?, AmountOfFactories = ?, AmountOfWholesales = ?, AmountOfDistributors = ?,AmountOfRetailers = ?,MinimalOrderRetail = ?, MaximumOrderRetail = ?, ContinuePlayingWhenBankrupt = ?, InsightFacilities = ? WHERE GameId = ?";
 	private static final String DELETE_CONFIGURATION = "";
 
-	public void createConfiguration(String GameName, Date GameEndDate, int AmountOfRounds, int AmountOfFactories, int AmountOfWholesales, int AmountOfDistributors, int AmountOfRetailers, int MinimalOrderRetail, int MaximumOrderRetail, boolean ContinuePlayingWhenBankrupt, boolean InsightFacilities){
+	public void createConfiguration(String GameId, int AmountOfRounds, int AmountOfFactories, int AmountOfWholesales, int AmountOfDistributors, int AmountOfRetailers, int MinimalOrderRetail, int MaximumOrderRetail, boolean ContinuePlayingWhenBankrupt, boolean InsightFacilities) {
 		Connection conn = null;
 		try {
 			conn = connect();
@@ -29,18 +28,16 @@ public class ConfigurationDAO {
 
 				conn.setAutoCommit(false);
 
-				pstmt.setString(1, GameName);
-				pstmt.setString(2, new Date().toString());
-				pstmt.setString(3, GameEndDate.toString());
-				pstmt.setInt(3,AmountOfRounds);
-				pstmt.setInt(4,AmountOfFactories);
-				pstmt.setInt(5,AmountOfWholesales);
-				pstmt.setInt(6,AmountOfDistributors);
-				pstmt.setInt(7,AmountOfRetailers);
-				pstmt.setInt(8,MinimalOrderRetail);
-				pstmt.setInt(9,MaximumOrderRetail);
-				pstmt.setBoolean(10,ContinuePlayingWhenBankrupt);
-				pstmt.setBoolean(11,InsightFacilities);
+				pstmt.setString(1, GameId);
+				pstmt.setInt(2, AmountOfRounds);
+				pstmt.setInt(3, AmountOfFactories);
+				pstmt.setInt(4, AmountOfWholesales);
+				pstmt.setInt(5, AmountOfDistributors);
+				pstmt.setInt(6, AmountOfRetailers);
+				pstmt.setInt(7, MinimalOrderRetail);
+				pstmt.setInt(8, MaximumOrderRetail);
+				pstmt.setBoolean(9, ContinuePlayingWhenBankrupt);
+				pstmt.setBoolean(10, InsightFacilities);
 
 				pstmt.executeUpdate();
 			}
@@ -51,7 +48,7 @@ public class ConfigurationDAO {
 		}
 	}
 
-	public List<Configuration> readConfigurations(){
+	public List<Configuration> readConfigurations() {
 		Connection conn = null;
 		ArrayList<Configuration> configurations = new ArrayList<>();
 		try {
@@ -63,7 +60,7 @@ public class ConfigurationDAO {
 					while (rs.next()) {
 						configurations.add(new Configuration(rs.getString("GameId"), rs.getInt("AmountOfRounds"),
 								rs.getInt("AmoundOfFactories"), rs.getInt("AmountOfWholesales"),
-								rs.getInt("AmountOfDistributors"),rs.getInt("AmountOfRetailers"),
+								rs.getInt("AmountOfDistributors"), rs.getInt("AmountOfRetailers"),
 								rs.getInt("MinimalOrderRetail"), rs.getInt("MaximalOrderRetail"),
 								rs.getBoolean("ContinuePlayingWhileBankrupt"), rs.getBoolean("InsightFacilities")));
 					}
@@ -75,4 +72,26 @@ public class ConfigurationDAO {
 		return configurations;
 	}
 
+	public void updateConfigurations(Configuration configuration) {
+		Connection conn = null;
+		try {
+			conn = connect();
+			if (conn == null)
+				try (PreparedStatement pstmt = conn.prepareStatement(UPDATE_CONFIGURATION)) {
+					conn.setAutoCommit(false);
+					pstmt.setInt(1, configuration.getAmountOfRounds());
+					pstmt.setInt(2, configuration.getAmountOfFactories());
+					pstmt.setInt(3, configuration.getAmountOfWholesales());
+					pstmt.setInt(4, configuration.getAmountOfDistributors());
+					pstmt.setInt(5, configuration.getAmountOfRetailers());
+					pstmt.setInt(6, configuration.getMinimalOrderRetail());
+					pstmt.setInt(7, configuration.getMaximumOrderRetail());
+					pstmt.setBoolean(8, configuration.isContinuePlayingWhenBankrupt());
+					pstmt.setBoolean(9, configuration.isInsightFacilities());
+					pstmt.setString(10, configuration.getGameId());
+				}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
