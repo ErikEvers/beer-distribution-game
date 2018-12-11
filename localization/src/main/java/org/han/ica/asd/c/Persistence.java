bef71e3dfed7bf83ebe191ad0a28f1274d6d03c9
Carsten Flokstra <C.Flokstra@student.han.nl>
2018-12-11 11:12:43 +0100

    Initial skeleton of the Persistence class according to the IPersistence interface

---
+++ localization/src/main/java/org/han/ica/asd/c/Persistence.java
@@ -0,0 +1,37 @@
+package org.han.ica.asd.c;
+
+public class Persistence implements IPersistence {
+
+	public void storeRoundData(Round rounddata)
+	{
+
+	}
+
+	public Beergame getGameLog(String gameID)
+	{
+
+	}
+
+	public Round getRoundData(String gameID)
+	{
+
+
+	}
+
+	public void logUsedBusinessRuleToCreateOrder(int facilityID, String gameID,String businessRule,
+												 FacilityLinkedTo facility, int outGoingOrderAmount)
+	{
+
+
+	}
+
+	public Turn fetchTurnData()
+	{
+
+	}
+
+	public void saveTurnData(Turn turn)
+	{
+
+
+	}}
