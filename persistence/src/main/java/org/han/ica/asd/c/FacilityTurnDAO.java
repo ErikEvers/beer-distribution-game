d31b665e6129a6bfc5549f3c06c988e33798a1a9
Carsten Flokstra <C.Flokstra@student.han.nl>
2018-12-12 13:09:45 +0100

    Made some little refactors in the FacilityTurnDAO.java

--- persistence/src/main/java/org/han/ica/asd/c/FacilityTurnDAO.java
+++ persistence/src/main/java/org/han/ica/asd/c/FacilityTurnDAO.java
@@ -1,6 +1,8 @@
 package org.han.ica.asd.c;
 
+import org.han.ica.asd.c.model.FacilityLinkedTo;
 import org.han.ica.asd.c.model.FacilityTurn;
+import org.han.ica.asd.c.model.Round;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -17,40 +19,43 @@ public class FacilityTurnDAO implements IBeerDisitributionGameDAO {
 	private static final String CREATE_TURN = "INSERT INTO FacilityTurn VALUES (?,?,?,?,?,?,?,?,?);";
 	private static final String UPDATE_TURN = "UPDATE FacilityTurn SET Stock = ?,RemainingBudget = ?,OrderAmount = ?, OpenOrderAmount = ?, OutgoingGoodsAmount = ? WHERE GameId = ? && RoundId = ? && FacilityIdOrder = ? && FacilityIdDeliver = ?)";
 	private static final String READ_TURNS = "SELECT * FROM FacilityTurn WHERE GameId = ? && RoundId = ?;";
+	public static final String READ_TURN = "SELECT FROM FacilityTurn WHERE GameId = ? && RoundId = ? && FacilityIdOrder = ? && FacilityIdDeliver = ?;";
 	private static final String DELETE_TURN = "DELETE FROM FacilityTurn WHERE GameId = ? && RoundId = ? && FacilityIdOrder = ? && FacilityIdDeliver = ?;";
 	private static final Logger LOGGER = Logger.getLogger(FacilityTurnDAO.class.getName());
 
 	/**
 	 * A method to create a FacilityTurn in the SQLite Database
+	 *
 	 * @param facilityTurn A FacilityTurn object which contains the data which needs to be inserted in the SQLite Database
 	 */
-	public void createTurn(FacilityTurn facilityTurn){
+	public void createTurn(FacilityTurn facilityTurn) {
 		Connection conn;
 		try {
 			conn = connect();
 			try (PreparedStatement pstmt = conn.prepareStatement(CREATE_TURN)) {
 
-				pstmt.setString(1,facilityTurn.getGameId());
-				pstmt.setInt(2,facilityTurn.getRoundId());
+				pstmt.setString(1, facilityTurn.getGameId());
+				pstmt.setInt(2, facilityTurn.getRoundId());
 				pstmt.setInt(3, facilityTurn.getFacilityIdOrder());
-				pstmt.setInt(4,facilityTurn.getFacilityIdDeliver());
-				pstmt.setInt(5,facilityTurn.getStock());
-				pstmt.setInt(6,facilityTurn.getRemainingBudget());
-				pstmt.setInt(7,facilityTurn.getOrder());
-				pstmt.setInt(8,facilityTurn.getOpenOrder());
-				pstmt.setInt(9,facilityTurn.getOutgoingGoods());
+				pstmt.setInt(4, facilityTurn.getFacilityIdDeliver());
+				pstmt.setInt(5, facilityTurn.getStock());
+				pstmt.setInt(6, facilityTurn.getRemainingBudget());
+				pstmt.setInt(7, facilityTurn.getOrder());
+				pstmt.setInt(8, facilityTurn.getOpenOrder());
+				pstmt.setInt(9, facilityTurn.getOutgoingGoods());
 
 				pstmt.executeUpdate();
 			}
 			conn.commit();
 		} catch (SQLException e) {
-			LOGGER.log(Level.SEVERE,e.toString(),e);
+			LOGGER.log(Level.SEVERE, e.toString(), e);
 		}
 	}
 
 	/**
 	 * A method which returns all the turns from a specific round from the SQLite Database
-	 * @param gameId The id of the game of the turn which needs to be returned
+	 *
+	 * @param gameId  The id of the game of the turn which needs to be returned
 	 * @param roundId The id of the specific round which needs to be returned
 	 * @return Returns a list of all the turns in a specific round in a specific game
 	 */
@@ -71,63 +76,90 @@ public class FacilityTurnDAO implements IBeerDisitributionGameDAO {
 					}
 				}
 			}
-			} catch(SQLException e){
-				LOGGER.log(Level.SEVERE, e.toString());
-			}
-			return turns;
+		} catch (SQLException e) {
+			LOGGER.log(Level.SEVERE, e.toString());
+		}
+		return turns;
 
+	}
+
+	public FacilityTurn fetchTurn(Round round, FacilityLinkedTo facilityLinkedTo) {
+		Connection conn;
+		FacilityTurn facilityTurn = null;
+		try {
+			conn = connect();
+			if (conn != null) {
+				try (PreparedStatement pstmt = conn.prepareStatement(READ_TURN)) {
+					pstmt.setString(1, round.getGameId());
+					pstmt.setInt(2, round.getRoundId());
+					pstmt.setInt(3, facilityLinkedTo.getFacilityIdOrder());
+					pstmt.setInt(4, facilityLinkedTo.getFacilityIdDeliver());
+					try (ResultSet rs = pstmt.executeQuery()) {
+						facilityTurn = new FacilityTurn(rs.getString("GameId"), rs.getInt("RoundId"), rs.getInt("FaciltyIdOrder"), rs.getInt("FacilityIdDeliver"), rs.getInt("Stock"), rs.getInt("RemainingBudget"), rs.getInt("OrderAmount"), rs.getInt("OpenOrderAmount"), rs.getInt("OutgoingGoodsAmount"));
+
+					} catch (SQLException e) {
+						LOGGER.log(Level.SEVERE, e.toString(), e);
+					}
+				}
+			}
+		} catch (SQLException e) {
+			LOGGER.log(Level.SEVERE, e.toString());
+		}
+		return facilityTurn;
 	}
 
 
 	/**
 	 * A method which updates a specific turn in the SQLite Database
+	 *
 	 * @param facilityTurn A FacilityTurn object which contains the data which needs to be updated in the SQLite Database
 	 */
-	public void updateTurn(FacilityTurn facilityTurn){
+	public void updateTurn(FacilityTurn facilityTurn) {
 		Connection conn;
 		try {
 			conn = connect();
 			if (conn != null) {
-			try (PreparedStatement pstmt = conn.prepareStatement(UPDATE_TURN)) {
+				try (PreparedStatement pstmt = conn.prepareStatement(UPDATE_TURN)) {
 
-				pstmt.setInt(1,facilityTurn.getStock());
-				pstmt.setInt(2,facilityTurn.getRemainingBudget());
-				pstmt.setInt(3,facilityTurn.getOrder());
-				pstmt.setInt(4,facilityTurn.getOpenOrder());
-				pstmt.setInt(5,facilityTurn.getOutgoingGoods());
-				pstmt.setString(6,facilityTurn.getGameId());
-				pstmt.setInt(7,facilityTurn.getRoundId());
-				pstmt.setInt(8, facilityTurn.getFacilityIdOrder());
-				pstmt.setInt(9,facilityTurn.getFacilityIdDeliver());
+					pstmt.setInt(1, facilityTurn.getStock());
+					pstmt.setInt(2, facilityTurn.getRemainingBudget());
+					pstmt.setInt(3, facilityTurn.getOrder());
+					pstmt.setInt(4, facilityTurn.getOpenOrder());
+					pstmt.setInt(5, facilityTurn.getOutgoingGoods());
+					pstmt.setString(6, facilityTurn.getGameId());
+					pstmt.setInt(7, facilityTurn.getRoundId());
+					pstmt.setInt(8, facilityTurn.getFacilityIdOrder());
+					pstmt.setInt(9, facilityTurn.getFacilityIdDeliver());
 
-				pstmt.executeUpdate();
-			}
-			conn.commit();
+					pstmt.executeUpdate();
+				}
+				conn.commit();
 			}
 		} catch (SQLException e) {
-			LOGGER.log(Level.SEVERE,e.toString());
+			LOGGER.log(Level.SEVERE, e.toString());
 		}
 	}
 
 	/**
 	 * A method which deletes a specific turn in the SQLite Database
+	 *
 	 * @param facilityTurn A FacilityTurn object which contains the data which needs to be inserted in the SQLite Database
 	 */
-	public void deleteTurn(FacilityTurn facilityTurn){
+	public void deleteTurn(FacilityTurn facilityTurn) {
 		Connection conn;
 		try {
 			conn = connect();
 			try (PreparedStatement pstmt = conn.prepareStatement(DELETE_TURN)) {
-				pstmt.setString(1,facilityTurn.getGameId());
-				pstmt.setInt(2,facilityTurn.getRoundId());
-				pstmt.setInt(3,facilityTurn.getFacilityIdOrder());
-				pstmt.setInt(4,facilityTurn.getFacilityIdDeliver());
+				pstmt.setString(1, facilityTurn.getGameId());
+				pstmt.setInt(2, facilityTurn.getRoundId());
+				pstmt.setInt(3, facilityTurn.getFacilityIdOrder());
+				pstmt.setInt(4, facilityTurn.getFacilityIdDeliver());
 
 				pstmt.executeUpdate();
 			}
 			conn.commit();
 		} catch (SQLException e) {
-			LOGGER.log(Level.SEVERE,e.toString());
+			LOGGER.log(Level.SEVERE, e.toString());
 		}
 	}
 }
