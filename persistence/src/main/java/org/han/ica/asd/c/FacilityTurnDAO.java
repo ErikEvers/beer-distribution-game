a64ca34134ea1c7a34a41e042c75330d4fbafb5b
Carsten Flokstra <C.Flokstra@student.han.nl>
2018-12-12 10:58:18 +0100

    Added the createTurn() method

--- persistence/src/main/java/org/han/ica/asd/c/FacilityTurnDAO.java
+++ persistence/src/main/java/org/han/ica/asd/c/FacilityTurnDAO.java
@@ -13,9 +13,34 @@ import static org.han.ica.asd.c.dbconnection.DBConnection.connect;
 
 public class FacilityTurnDAO {
 	private static final String READ_TURNS = "SELECT* FROM FacilityTurn WHERE GameId = ? && RoundId = ?;";
+	private static final String CREATE_TURN = "INSERT INTO FacilityTurn VALUES (?,?,?,?,?,?,?,?,?)";
+
+	public void createTurn(FacilityTurn facilityTurn){
+		Connection conn;
+			try {
+				conn = connect();
+				try (PreparedStatement pstmt = conn.prepareStatement(CREATE_TURN)) {
+					pstmt.setString(1,facilityTurn.getGameId());
+					pstmt.setInt(2,facilityTurn.getRoundId());
+					pstmt.setInt(3, facilityTurn.getFacilityIdOrder());
+					pstmt.setInt(4,facilityTurn.getFacilityIdDeliver());
+					pstmt.setInt(5,facilityTurn.getStock());
+					pstmt.setInt(6,facilityTurn.getRemainingBudget());
+					pstmt.setInt(7,facilityTurn.getOrder());
+					pstmt.setInt(8,facilityTurn.getOpenOrder());
+					pstmt.setInt(9,facilityTurn.getOutgoingGoods());
+
+					pstmt.executeUpdate();
+				}
+				conn.commit();
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
 
 	public List<FacilityTurn> fetchTurns(String gameId, int roundId) {
-		Connection conn = null;
+		Connection conn;
 		ArrayList<FacilityTurn> turns = new ArrayList<>();
 		try {
 			conn = connect();
