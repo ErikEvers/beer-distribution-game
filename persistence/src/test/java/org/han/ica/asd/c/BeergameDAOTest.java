d05cd211b62ad72b6d0e042e0b81bb2a44610cf6
Carsten Flokstra <C.Flokstra@student.han.nl>
2018-12-12 16:36:00 +0100

    Generated skeleton for BeergameDAOTest.java

---
+++ persistence/src/test/java/org/han/ica/asd/c/BeergameDAOTest.java
@@ -0,0 +1,34 @@
+package org.han.ica.asd.c;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeergameDAOTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void createBeergame() {
+	}
+
+	@Test
+	void readBeergames() {
+	}
+
+	@Test
+	void deleteBeergame() {
+	}
+
+	@Test
+	void getGameLog() {
+	}
+}
