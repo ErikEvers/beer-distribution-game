b29b7de9565caf58120ae994572fbd3c4c6d07c7
Carsten Flokstra <C.Flokstra@student.han.nl>
2018-12-12 10:45:41 +0100

    Implemented the fetchTurns() method correctly now

--- persistence/src/main/java/org/han/ica/asd/c/FacilityTurnDAO.java
+++ persistence/src/main/java/org/han/ica/asd/c/FacilityTurnDAO.java
@@ -4,10 +4,11 @@ import org.han.ica.asd.c.model.FacilityTurn;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.han.ica.asd.c.dbconnection.DBConnection.RollBackTransaction;
 import static org.han.ica.asd.c.dbconnection.DBConnection.connect;
 
 public class FacilityTurnDAO {
@@ -15,22 +16,22 @@ public class FacilityTurnDAO {
 
 	public List<FacilityTurn> fetchTurns(String gameId, int roundId) {
 		Connection conn = null;
+		ArrayList<FacilityTurn> turns = new ArrayList<>();
 		try {
 			conn = connect();
-			if (conn == null) return;
+			if (conn == null) return null;
 			try (PreparedStatement pstmt = conn.prepareStatement(READ_TURNS)) {
-
-				conn.setAutoCommit(false);
-
-				pstmt.setString(1, gameId);
-				pstmt.setInt(2, roundId);
-
-				pstmt.executeUpdate();
+				pstmt.setString(1,gameId);
+				pstmt.setInt(2,roundId);
+				try (ResultSet rs = pstmt.executeQuery()) {
+					while (rs.next()) {
+						turns.add(new FacilityTurn(rs.getString("GameId"), rs.getInt("RoundId"), rs.getInt("FaciltyIdOrder"), rs.getInt("FacilityIdDeliver"),rs.getInt("Stock"),rs.getInt("RemainingBudget"),rs.getInt("OrderAmount"), rs.getInt("OpenOrderAmount"),rs.getInt("OutgoingGoodsAmount")));
+					}
+				}
 			}
-			conn.commit();
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
-			RollBackTransaction(conn);
 		}
+		return turns;
 	}
 }
