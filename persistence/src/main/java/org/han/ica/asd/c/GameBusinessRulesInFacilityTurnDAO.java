b4fdc09b65e7c3ce258d9ca18ff23d9c14fea1bb
Carsten Flokstra <C.Flokstra@student.han.nl>
2018-12-12 15:13:31 +0100

    Implemented the readTurn() method in the GameBusinessRulesInFacilityTurnDAO.java

--- persistence/src/main/java/org/han/ica/asd/c/GameBusinessRulesInFacilityTurnDAO.java
+++ persistence/src/main/java/org/han/ica/asd/c/GameBusinessRulesInFacilityTurnDAO.java
@@ -4,6 +4,7 @@ import org.han.ica.asd.c.model.GameBusinessRulesInFacilityTurn;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,7 +12,9 @@ import java.util.logging.Logger;
 import static org.han.ica.asd.c.dbconnection.DBConnection.connect;
 
 public class GameBusinessRulesInFacilityTurnDAO implements IBeerDisitributionGameDAO {
-	public static final String CREATE_BUSINESSRULETURN = "INSERT INTO GameBusinessRuleInFacility VALUES (?,?,?,?,?,?,?);";
+	private static final String CREATE_BUSINESSRULETURN = "INSERT INTO GameBusinessRuleInFacility VALUES (?,?,?,?,?,?,?);";
+	private static final String READ_BUSINESSRULETURN = "SELECT FROM GameBusinessRuleInFacility WHERE GameId = ? && RoundId = ? && FacillityIdOrder = ?, FacilityIdDeliver = ? ;";
+
 	private static final Logger LOGGER = Logger.getLogger(GameBusinessRulesInFacilityTurnDAO.class.getName());
 
 	public void createTurn(GameBusinessRulesInFacilityTurn gameBusinessRulesInFacilityTurn) {
@@ -20,8 +23,8 @@ public class GameBusinessRulesInFacilityTurnDAO implements IBeerDisitributionGam
 			conn = connect();
 			try (PreparedStatement pstmt = conn.prepareStatement(CREATE_BUSINESSRULETURN)) {
 				pstmt.setInt(1,gameBusinessRulesInFacilityTurn.getRoundId());
-				pstmt.setInt(2,gameBusinessRulesInFacilityTurn.getFacilityOrder());
-				pstmt.setInt(3,gameBusinessRulesInFacilityTurn.getFacilityDeliver());
+				pstmt.setInt(2,gameBusinessRulesInFacilityTurn.getFacilityIdOrder());
+				pstmt.setInt(3,gameBusinessRulesInFacilityTurn.getFacilityIdDeliver());
 				pstmt.setString(4,gameBusinessRulesInFacilityTurn.getGameId());
 				pstmt.setString(5,gameBusinessRulesInFacilityTurn.getGameAgentName());
 				pstmt.setString(6,gameBusinessRulesInFacilityTurn.getGameAST());
@@ -32,5 +35,38 @@ public class GameBusinessRulesInFacilityTurnDAO implements IBeerDisitributionGam
 		} catch (SQLException e) {
 			LOGGER.log(Level.SEVERE, e.toString(), e);
 		}
+	}
+
+	public GameBusinessRulesInFacilityTurn readTurn(String gameId, int roundId, int facililtyIdOrder, int facilityIdDeliver){
+		Connection conn;
+		GameBusinessRulesInFacilityTurn gameBusinessRulesInFacilityTurn = null;
+		try {
+			conn = connect();
+			try (PreparedStatement pstmt = conn.prepareStatement(READ_BUSINESSRULETURN)) {
+				pstmt.setString(1,gameId);
+				pstmt.setInt(2,roundId);
+				pstmt.setInt(3,facililtyIdOrder);
+				pstmt.setInt(4,facilityIdDeliver);
+
+
+				pstmt.executeUpdate();
+				gameBusinessRulesInFacilityTurn = getGame(pstmt);
+			}
+			conn.commit();
+		} catch (SQLException e) {
+			LOGGER.log(Level.SEVERE, e.toString(), e);
+		}
+	return gameBusinessRulesInFacilityTurn;
+	}
+
+	private GameBusinessRulesInFacilityTurn getGame(PreparedStatement pstmt) {
+		GameBusinessRulesInFacilityTurn gameBusinessRulesInFacilityTurn = null;
+		try (ResultSet rs = pstmt.executeQuery()) {
+			gameBusinessRulesInFacilityTurn = new GameBusinessRulesInFacilityTurn(rs.getInt("RoundId"), rs.getInt("FaciltyIdOrder"), rs.getInt("FacilityIdDeliver"),rs.getString("GameId"),  rs.getString("AgentName"), rs.getString("GameBusinessRule"), rs.getString("GameAST"));
+
+		} catch (SQLException e) {
+			LOGGER.log(Level.SEVERE, e.toString(), e);
+		}
+		return gameBusinessRulesInFacilityTurn;
 	}
 }
