e112489676631af986298eb48bbfb1a8fcdeb10f
Carsten Flokstra <C.Flokstra@student.han.nl>
2018-12-12 11:37:28 +0100

    Some refactoring and added some Javadocs

--- persistence/src/main/java/org/han/ica/asd/c/RoundDAO.java
+++ persistence/src/main/java/org/han/ica/asd/c/RoundDAO.java
@@ -16,27 +16,51 @@ public class RoundDAO {
 	private FacilityTurnDAO turnDAO;
 
 
+	/**
+	 * A method to create a round in the SQLite Database
+	 * @param gameId The id of the game of the new round
+	 * @param roundId The id of the round that the players have played
+	 */
 	public void createRound(String gameId, int roundId){
 		Connection conn = null;
-		createPreparedStatement(gameId, roundId, conn, CREATE_ROUND);
+		executePreparedStatement(gameId, roundId, conn, CREATE_ROUND);
 	}
 
+	/**
+	 * A method which deletes a specific round in the SQLite Database
+	 * @param gameId The id of the game of the round which needs to be deleted
+	 * @param roundId The id of the round which needs to be deleted
+	 */
 	public void deleteRound(String gameId, int roundId){
 		Connection conn = null;
-		createPreparedStatement(gameId, roundId, conn, DELETE_ROUND);
+		executePreparedStatement(gameId, roundId, conn, DELETE_ROUND);
 	}
 
+	/**
+	 * A method which returns a specific round with all the turns from the SQLite Database
+	 * @param gameId The id of the game of the round which needs to be returned
+	 * @param roundId The id of the specific round which needs to be returned
+	 * @return A round object with turns
+	 */
 	public Round getRound(String gameId, int roundId){
 		Round round = new Round(gameId,roundId);
 		round.setTurns(turnDAO.fetchTurns(gameId,roundId));
 		return round;
 	}
 
-	private void createPreparedStatement(String gameId, int roundId, Connection conn, String createRound) {
+
+	/**
+	 * A helper method to execute a prepared statement for the SQLite Database.
+	 * @param gameId The id of a specific game
+	 * @param roundId The id of a specific round
+	 * @param conn A connection object which can be used to execute the statement
+	 * @param sqlStatement The statement which needs to be executed
+	 */
+	private void executePreparedStatement(String gameId, int roundId, Connection conn, String sqlStatement) {
 		try {
 			conn = connect();
 			if (conn == null) return;
-			try (PreparedStatement pstmt = conn.prepareStatement(createRound)) {
+			try (PreparedStatement pstmt = conn.prepareStatement(sqlStatement)) {
 
 				conn.setAutoCommit(false);
 
