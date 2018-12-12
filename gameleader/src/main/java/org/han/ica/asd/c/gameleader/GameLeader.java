a162a2eae8ecaad38e025998e545f19376181020
kaspervdw <kaspervdw@live.nl>
2018-12-12 15:37:19 +0100

    added initial gameleader and turnhandler classes

--- gameleader/src/main/java/org/han/ica/asd/c/gameleader/GameLeader.java
+++ gameleader/src/main/java/org/han/ica/asd/c/gameleader/GameLeader.java
@@ -1,4 +1,33 @@
-package org.han.ica.asd.c;
+package org.han.ica.asd.c.gameleader;
 
-public class GameLeader {
+import org.han.ica.asd.c.gameleader.componentInterfaces.IConnectorForLeader;
+import org.han.ica.asd.c.model.BeerGame;
+import org.han.ica.asd.c.model.FacilityTurn;
+import org.han.ica.asd.c.model.Round;
+import org.han.ica.asd.c.observers.ITurnModelObserver;
+
+import javax.inject.Inject;
+
+public class GameLeader implements ITurnModelObserver {
+    @Inject
+    private IConnectorForLeader connectorForLeader;
+
+    private TurnHandler turnHandler;
+    private Round currentRoundData;
+
+    private int turnsExpected;
+    private int turnsReceived;
+
+    public GameLeader() {
+        connectorForLeader.addObserver(this);
+        this.turnHandler = new TurnHandler();
+    }
+
+    public void turnModelReceived(FacilityTurn turnModel) {
+        turnHandler.processFacilityTurn(turnModel);
+        currentRoundData.addTurn(turnModel);
+        turnsReceived++;
+
+        if(turnsReceived == turnsExpected) allTurnsReceived(currentRoundData);
+    }
 }
--- gameleader/src/main/java/org/han/ica/asd/c/gameleader/TurnHandler.java
+++ gameleader/src/main/java/org/han/ica/asd/c/gameleader/TurnHandler.java
@@ -1,4 +1,26 @@
 package org.han.ica.asd.c.gameleader;
 
+import org.han.ica.asd.c.gameleader.componentInterfaces.IPersistence;
+import org.han.ica.asd.c.model.FacilityTurn;
+
+import javax.inject.Inject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class TurnHandler {
+    private static final Logger LOGGER = Logger.getLogger(TurnHandler.class.getName() );
+    @Inject
+    private IPersistence persistenceLayer;
+
+    protected void processFacilityTurn(FacilityTurn turnModel) {
+        if(validateFacilityTurn(turnModel)) {
+            persistenceLayer.savePlayerTurn(turnModel);
+        } else {
+            LOGGER.log(Level.WARNING, "Incoming orders can't be higher than the available stock.");
+        }
+    }
+
+    private boolean validateFacilityTurn(FacilityTurn turnModel) {
+        return (turnModel.getOrder() < turnModel.getStock() && turnModel.getOrder() >= 0);
+    }
 }
