3cc8389cdcf4dfde588ffed2eb1f26c6d8ce1b39
Chico Gerrits <CAGG.Gerrits@student.han.nl>
2018-12-12 14:04:26 +0100

    fixed magic numbers in main

--- gui_play_game/src/main/java/org/han/ica/asd/c/Main.java
+++ gui_play_game/src/main/java/org/han/ica/asd/c/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/PlayGameFactory.fxml"));
-        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
