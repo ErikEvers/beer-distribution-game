89a40fbe3c188c5cfdb35b08d5b68ff62c21daa9
Carsten Flokstra <C.Flokstra@student.han.nl>
2018-12-13 17:33:58 +0100

    Generated the skeleton of the GameBusinessRulesInFacilityTurnDAOIntegrationTest.java

---
+++ persistence/src/test/java/org/han/ica/asd/c/GameBusinessRulesInFacilityTurnDAOIntegrationTest.java
@@ -0,0 +1,30 @@
+package org.han.ica.asd.c;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameBusinessRulesInFacilityTurnDAOIntegrationTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void createTurnTest() {
+	}
+
+	@Test
+	void readTurnTest() {
+	}
+
+	@Test
+	void deleteTurnTest() {
+	}
+}
