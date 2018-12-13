d74b62d7fae74b3c0e80967c4b17a47337b855fb
Jamiel van Engen <JL.vanEngen@student.han.nl>
2018-12-13 10:46:17 +0100

    Added JavaDoc comments for the ParticipantsPool class

--- gamelogic/src/main/java/org/han/ica/asd/c/participants/ParticipantsPool.java
+++ gamelogic/src/main/java/org/han/ica/asd/c/participants/ParticipantsPool.java
@@ -7,6 +7,9 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * The ParticipantsPool manages the local participants.
+ */
 public class ParticipantsPool {
     private List<IParticipant> participants;
     private PlayerParticipant player;
@@ -17,19 +20,34 @@ public class ParticipantsPool {
         player = playerParticipant;
     }
 
+    /**
+     * Adds a participant to the pool.
+     * @param participant
+     */
     public void addParticipant(IParticipant participant) {
         participants.add(participant);
     }
 
+    /**
+     * Returns the lost of participants.
+     * @return
+     */
     public List<IParticipant> getParticipants() {
         return participants;
     }
 
+    /**
+     * Replaces the local player with the given agent.
+     * @param agent The agent to replace the local player with.
+     */
     public void replacePlayerWithAgent(AgentParticipant agent) {
         participants.remove(player);
         participants.add(agent);
     }
 
+    /**
+     * Replaces the agent of the local player with the local player.
+     */
     public void replaceAgentWithPlayer() {
         replaceParticipantWithPlayer(player);
     }
@@ -38,6 +56,11 @@ public class ParticipantsPool {
         replaceParticipantWithPlayer(playerParticipant);
     }
 
+    /**
+     * Replaces the agent that has the same facilityId as the given player.
+     * This method is used when a player gets back in to the game.
+     * @param playerParticipant The player that got back into the game.
+     */
     private void replaceParticipantWithPlayer(PlayerParticipant playerParticipant) {
         for (IParticipant participant : participants) {
             if (participant.getParticipantId() == playerParticipant.getParticipantId()) {
@@ -48,6 +71,10 @@ public class ParticipantsPool {
         }
     }
 
+    /**
+     * Removes the participant with the given participantId.
+     * @param participantId Identifier of the participant to remove from the pool.
+     */
     public void removeParticipant(int participantId) {
         for (IParticipant participant : participants) {
             if (participant.getParticipantId() == participantId) {
