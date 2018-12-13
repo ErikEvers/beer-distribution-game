7a35dfbd4b4a12aa29c56a8cc49b1392a80a0460
Chico Gerrits <CAGG.Gerrits@student.han.nl>
2018-12-13 13:43:29 +0100

    Added some button functionality

--- gui_play_game/src/main/java/org/han/ica/asd/c/MainMenuController.java
+++ gui_play_game/src/main/java/org/han/ica/asd/c/MainMenuController.java
@@ -1,13 +1,34 @@
 package org.han.ica.asd.c;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.util.ResourceBundle;
 
 public class MainMenuController {
     @FXML
     private AnchorPane mainContainer;
+    @FXML
+    private Button createGameButton;
 
     public void initialize(){
         mainContainer.getChildren().addAll();
     }
+
+    public void handleCloseGameButtonClick(){
+        Platform.exit();
+    }
+
+    public void handleCreateGameButtonClick() throws Exception{
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("languageResources");
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/fxml/PlayGameFactory.fxml"), resourceBundle));
+        Stage stage = (Stage) createGameButton.getScene().getWindow();
+        stage.setScene(scene);
+    }
+
 }
