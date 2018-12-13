51501ad57310f155cd4c0bd280b8fac49683a14b
Jamiel van Engen <JL.vanEngen@student.han.nl>
2018-12-13 14:53:58 +0100

    Removed the unused exceptions

--- gamelogic/src/main/java/org/han/ica/asd/c/gamelogic/exceptions/RoundDataNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.han.ica.asd.c.gamelogic.exceptions;
-
-public class RoundDataNotFoundException extends Exception {
-    public RoundDataNotFoundException() {
-        super("There was no round data found with the given facility!");
-    }
-}
