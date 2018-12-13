c7520317dee3f25ef73199462d2304b01086d44d
Jelle Dimmendaal <jellecf.1@gmail.com>
2018-12-13 15:28:21 +0100

    Implemented all methods from SDD. Ready for Review

--- gui_replay_game/src/main/java/org/han/ica/asd/c/gui_replay_game/replay_data/IReplay.java
+++ gui_replay_game/src/main/java/org/han/ica/asd/c/gui_replay_game/replay_data/IReplay.java
@@ -1,5 +1,13 @@
 package org.han.ica.asd.c.gui_replay_game.replay_data;
 
+import org.han.ica.asd.c.model.Round;
+
+import java.util.List;
+
 public interface IReplay {
-    void showGraphOverview(String gameId);
+    List<Round> showGraphOverview(String gameId);
+    //TODO: implementeer showGraphDetails() Onbekend wat de "details" nou zijn
+    List<Round> showRoundGraph(String gameId, int roundId);
+    List<Round> stepForward();
+    List<Round> stepBackwards();
 }
