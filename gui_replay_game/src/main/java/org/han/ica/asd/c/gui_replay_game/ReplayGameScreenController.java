77b146f30dbe6cab116f575e09ebd8e561e7233f
JordyVeldhuizen <j.veldhuizen@student.han.nl>
2018-12-17 13:50:19 +0100

    Small changes to the FXML document. Added first implementations in Controller. currentRound and totalRounds are filled with test data, this will be changed later on

--- gui_replay_game/src/main/java/org/han/ica/asd/c/gui_replay_game/ReplayGameScreenController.java
+++ gui_replay_game/src/main/java/org/han/ica/asd/c/gui_replay_game/ReplayGameScreenController.java
@@ -1,15 +1,19 @@
 package org.han.ica.asd.c.gui_replay_game;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
+
+import org.han.ica.asd.c.gui_replay_game.replay_data.ReplayData;
+import org.han.ica.asd.c.model.Facility;
 
 public class ReplayGameScreenController {
 
@@ -20,7 +24,7 @@ public class ReplayGameScreenController {
     private URL location;
 
     @FXML
-    private LineChart<?, ?> replayGraph;
+    private LineChart<Double, Double> replayGraph;
 
     @FXML
     private CheckBox factoryCheckbox;
@@ -49,24 +53,90 @@ public class ReplayGameScreenController {
     @FXML
     private TextField totalRoundsTextfield;
 
+    private int currentRound;
+    private int totalRounds;
+    private TextFormatter textFormatter;
+
     @FXML
     public void currentRoundEntered(ActionEvent event) {
+        int currentRoundInput = Integer.parseInt(currentRoundTextfield.getText());
 
+        if (currentRoundInput > totalRounds) {
+            currentRound = totalRounds;
+        } else if (currentRoundInput < 1) {
+            currentRound = 1;
+        } else {
+            currentRound = currentRoundInput;
+        }
+        updateCurrentRound();
+        drawGraph();
     }
 
     @FXML
     public void nextRoundButtonClicked(ActionEvent event) {
-
+        if (currentRound < totalRounds) {
+            this.currentRound++;
+            updateCurrentRound();
+            drawGraph();
+        }
     }
 
     @FXML
     public void prevRoundButtonClicked(ActionEvent event) {
-
+        if (currentRound > 1) {
+            this.currentRound--;
+            updateCurrentRound();
+            drawGraph();
+        }
     }
 
     @FXML
     void initialize() {
 
+        this.setuptextFormatter();
+        currentRoundTextfield.setTextFormatter(textFormatter);
+
+        currentRound = 1;
+        totalRounds = 5;
+        totalRoundsTextfield.setText(totalRounds + "");
+
+
+        updateCurrentRound();
+        drawGraph();
+    }
+
+    private void updateCurrentRound() {
+        currentRoundTextfield.setText(currentRound + "");
+    }
+
+    private void drawGraph() {
+        ObservableList<XYChart.Series<Double, Double>> lineChartData = FXCollections.observableArrayList();
+        LineChart.Series<Double, Double> series1 = new LineChart.Series<>();
+        series1.setName("TestData");
+        for (int i = 0; i <= currentRound; i++) {
+            series1.getData().add(new XYChart.Data<>(new Double(i), new Double(i)));
+        }
+        lineChartData.add(series1);
+        replayGraph.setData(lineChartData);
+    }
+
+    private void setuptextFormatter() {
+        textFormatter = new TextFormatter<>(change -> {
+            if (!change.isContentChange()) {
+                return change;
+            }
+
+            sanitizeInput(change);
+
+            return change;
+        });
+    }
+
+    private void sanitizeInput(TextFormatter.Change change) {
+        String text = change.getControlNewText();
+        if (!(text.matches("[0-9]+"))) {
+            change.setText(change.getText().replaceAll("[^\\d.]", ""));
+        }
     }
 }
 
