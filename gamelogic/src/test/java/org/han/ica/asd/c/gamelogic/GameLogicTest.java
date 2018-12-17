a41c22b98297e4f03220d1731271dcf07e889d6a
Johan <PJM.Heij@student.han.nl>
2018-12-17 10:50:28 +0100

    SIXGJM-365 Commit voor leerdoel: Ter aanvulling van vorige commit. Alle stock berekeningen. De backlog berekening voor deze unit test nog achterwegen gelaten. Hier maak ik een nieuwe unittest voor. Verder logica fouten verwijderd en een demand faciliteit toegevoegd, anders bestelt niemand bij de retailer. Alle stock berekeningen worden hier uitgevoerd. Dus het leveren van de bestelling en het wegsturen van een order.

--- gamelogic/src/test/java/org/han/ica/asd/c/gamelogic/GameLogicTest.java
+++ gamelogic/src/test/java/org/han/ica/asd/c/gamelogic/GameLogicTest.java
@@ -89,34 +89,46 @@ public class GameLogicTest {
     @Test
     public void testIfCalculatingInventoryGoesCorrectly() {
         Facility manufacturer = new Facility("0", 0, "0", "0", "0");
-        Facility distributor = new Facility("0", 0, "0", "0", "0");
+        Facility regionalWarehouse = new Facility("0", 0, "0", "0", "0");
         Facility wholesale = new Facility("0", 0, "0", "0", "0");
         Facility retailer = new Facility("0", 0, "0", "0", "0");
+        Facility demand = new Facility("0", 0, "0", "0", "0");
 
         FacilityLinkedTo facilityLinkedTo = new FacilityLinkedTo("0", manufacturer, manufacturer, true);
-        FacilityLinkedTo facilityLinkedTo1 = new FacilityLinkedTo("0", manufacturer, distributor, true);
-        FacilityLinkedTo facilityLinkedTo2 = new FacilityLinkedTo("0", distributor, wholesale, true);
-        FacilityLinkedTo facilityLinkedTo3 = new FacilityLinkedTo("0", wholesale, retailer, true);
+        FacilityLinkedTo facilityLinkedTo1 = new FacilityLinkedTo("0", regionalWarehouse, manufacturer, true);
+        FacilityLinkedTo facilityLinkedTo2 = new FacilityLinkedTo("0", wholesale, regionalWarehouse, true);
+        FacilityLinkedTo facilityLinkedTo3 = new FacilityLinkedTo("0", retailer, wholesale, true);
+        FacilityLinkedTo facilityLinkedTo4 = new FacilityLinkedTo("0", demand, retailer, true);
 
         gameLogic.addfacilities(facilityLinkedTo);
         gameLogic.addfacilities(facilityLinkedTo1);
         gameLogic.addfacilities(facilityLinkedTo2);
         gameLogic.addfacilities(facilityLinkedTo3);
+        gameLogic.addfacilities(facilityLinkedTo4);
 
         Round round = new RoundFake("0", 0);
         round.addTurnOrder(manufacturer, manufacturer, 25);
-        round.addTurnOrder(manufacturer, distributor, 0);
-        round.addTurnOrder(distributor, wholesale, 30);
-        round.addTurnOrder(wholesale, retailer, 15);
+        round.addTurnOrder(regionalWarehouse, manufacturer, 0);
+        round.addTurnOrder(wholesale, regionalWarehouse, 30);
+        round.addTurnOrder(retailer, wholesale, 15);
+        round.addTurnOrder(demand, retailer, 30);
 
         round.addFacilityStock(40, manufacturer);
-        round.addFacilityStock(40, distributor);
+        round.addFacilityStock(40, regionalWarehouse);
         round.addFacilityStock(40, wholesale);
         round.addFacilityStock(40, retailer);
 
         round = gameLogic.calculateRound(round);
 
-        Assert.assertEquals(round.getStockByFacility(manufacturer), 15);
+        Assert.assertEquals(round.getStockByFacility(manufacturer), 65);
+        Assert.assertEquals(round.getStockByFacility(regionalWarehouse), 10);
+        Assert.assertEquals(round.getStockByFacility(wholesale), 55);
+        Assert.assertEquals(round.getStockByFacility(retailer), 25);
+
         Assert.assertEquals(round.getTurnDeliverByFacility(manufacturer, manufacturer), 25);
+        Assert.assertEquals(round.getTurnDeliverByFacility(regionalWarehouse, manufacturer), 0);
+        Assert.assertEquals(round.getTurnDeliverByFacility(wholesale, regionalWarehouse), 30);
+        Assert.assertEquals(round.getTurnDeliverByFacility(retailer, wholesale), 15);
+        Assert.assertEquals(round.getTurnDeliverByFacility(demand, retailer), 30);
     }
 }
