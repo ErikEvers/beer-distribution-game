19a0e21b53680b9e3d4e64c9ce86f3dd394c5117
Marnix Kortsilius <marnixkortsilius@hotmail.com>
2018-12-17 13:33:26 +0100

    WIP

--- persistence/src/test/java/org/han/ica/asd/c/FacilityTypeDAOIntegrationTest.java
+++ persistence/src/test/java/org/han/ica/asd/c/FacilityTypeDAOIntegrationTest.java
@@ -2,49 +2,59 @@ package org.han.ica.asd.c;
 
 import org.han.ica.asd.c.dbconnection.DBConnectionTest;
 import org.han.ica.asd.c.dbconnection.DatabaseConnection;
+import org.han.ica.asd.c.model.Configuration;
 import org.han.ica.asd.c.model.FacilityType;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class FacilityTypeDAOIntegrationTest {
     private static final Logger LOGGER = Logger.getLogger(FacilityTypeDAOIntegrationTest.class.getName());
-    private static final FacilityType FACILITY_TYPE = new FacilityType("Factory", "gameId1", 10, 10, 5, 5, 100, 5);
-    private static final FacilityType FACILITY_TYPE2 = new FacilityType("Wholesale", "gameId1", 15, 15, 10, 10, 150, 10);
-    private static final FacilityType FACILITY_TYPE2_UPDATE = new FacilityType("Wholesale", "gameId1", 20, 20, 20, 20, 20, 20);
+    private static final Configuration CONFIGURATION = new Configuration("BeergameTest", 5, 5, 5, 5, 5, 5, 10, true,false);
+    private static final FacilityType FACILITY_TYPE = new FacilityType("Factory", "BeergameTest", 10, 10, 5, 5, 100, 5);
+    private static final FacilityType FACILITY_TYPE2 = new FacilityType("Wholesale", "BeergameTest", 15, 15, 10, 10, 150, 10);
+    private static final FacilityType FACILITY_TYPE2_UPDATE = new FacilityType("Wholesale", "BeergameTest", 20, 20, 20, 20, 20, 20);
 
+    private BeergameDAO beergameDAO;
+    private ConfigurationDAO configurationDAO;
     private FacilityTypeDAO facilityTypeDAO;
 
-    @Before
+
+    @BeforeEach
     public void setUp() {
         DBConnectionTest.getInstance().createNewDatabase();
-        System.out.println("Setup");
     }
 
-    @After
+    @AfterEach
     public void tearDown() {
         DBConnectionTest.getInstance().cleanup();
-        System.out.println("Teardown");
+        System.out.println("MAAK DE DATABASE LEEG");
     }
 
     @Test
-    public void createFacilityTypeTest(){
+    public void createFacilityTypeTest() {
+        beergameDAO = new BeergameDAO();
+        configurationDAO = new ConfigurationDAO();
         facilityTypeDAO = new FacilityTypeDAO();
 
         DatabaseConnection connection = DBConnectionTest.getInstance();
-        setDatabaseConnection(facilityTypeDAO, connection);
+        setDatabaseConnection(beergameDAO, connection);
+        beergameDAO.createBeergame("BeergameTest");
+        Assert.assertEquals(1,beergameDAO.readBeergames().size());
 
+        setDatabaseConnection(configurationDAO, connection);
+        configurationDAO.createConfiguration(CONFIGURATION);
+        Assert.assertEquals(1,configurationDAO.readConfigurations().size());
+
+        setDatabaseConnection(facilityTypeDAO, connection);
         facilityTypeDAO.createFacilityType(FACILITY_TYPE);
 
-        FacilityType facilityTypeFromDB = facilityTypeDAO.readSpecificFacilityType("gameId1", "Factory");
+        FacilityType facilityTypeFromDB = facilityTypeDAO.readSpecificFacilityType(FACILITY_TYPE.getGameId(), FACILITY_TYPE.getFacilityName());
 
         Assert.assertEquals(FACILITY_TYPE.getFacilityName(), facilityTypeFromDB.getFacilityName());
         Assert.assertEquals(FACILITY_TYPE.getGameId(), facilityTypeFromDB.getGameId());
@@ -56,54 +66,13 @@ public class FacilityTypeDAOIntegrationTest {
         Assert.assertEquals(FACILITY_TYPE.getValueOutgoingGoods(), facilityTypeFromDB.getValueOutgoingGoods());
     }
 
-    @Test
-    public void updateFacilityTypeTest() {
-        facilityTypeDAO = new FacilityTypeDAO();
-
-        DatabaseConnection connection = DBConnectionTest.getInstance();
-        setDatabaseConnection(facilityTypeDAO, connection);
-
-        facilityTypeDAO.createFacilityType(FACILITY_TYPE2);
-        facilityTypeDAO.updateFacilityType(FACILITY_TYPE2_UPDATE);
-
-        FacilityType facilityTypeFromDB = facilityTypeDAO.readSpecificFacilityType("gameId1", "Wholesale");
-
-        Assert.assertEquals(FACILITY_TYPE2_UPDATE.getFacilityName(), facilityTypeFromDB.getFacilityName());
-        Assert.assertEquals(FACILITY_TYPE2_UPDATE.getGameId(), facilityTypeFromDB.getGameId());
-        Assert.assertEquals(FACILITY_TYPE2_UPDATE.getOpenOrderCosts(), facilityTypeFromDB.getOpenOrderCosts());
-        Assert.assertEquals(FACILITY_TYPE2_UPDATE.getStartingBudget(), facilityTypeFromDB.getStartingBudget());
-        Assert.assertEquals(FACILITY_TYPE2_UPDATE.getStartingOrder(), facilityTypeFromDB.getStartingOrder());
-        Assert.assertEquals(FACILITY_TYPE2_UPDATE.getStockHoldingCosts(), facilityTypeFromDB.getStockHoldingCosts());
-        Assert.assertEquals(FACILITY_TYPE2_UPDATE.getValueIncomingGoods(), facilityTypeFromDB.getValueIncomingGoods());
-        Assert.assertEquals(FACILITY_TYPE2_UPDATE.getValueOutgoingGoods(), facilityTypeFromDB.getValueOutgoingGoods());
-    }
-
-    @Test
-    public void deleteSpecificFacilityType() {
-        facilityTypeDAO = new FacilityTypeDAO();
-
-        DatabaseConnection connection = DBConnectionTest.getInstance();
-        setDatabaseConnection(facilityTypeDAO, connection);
-
-        facilityTypeDAO.createFacilityType(FACILITY_TYPE);
-
-        List<FacilityType> facilityTypeFromDB = facilityTypeDAO.readAllFacilityTypes(FACILITY_TYPE.getGameId());
-
-        Assert.assertEquals(1, facilityTypeFromDB.size());
-
-        facilityTypeDAO.deleteSpecificFacilityType(FACILITY_TYPE.getGameId(), FACILITY_TYPE.getFacilityName());
-
-        List<FacilityType> facilityTypeFromDBAfterDelete = facilityTypeDAO.readAllFacilityTypes(FACILITY_TYPE.getGameId());
-
-        Assert.assertEquals(0, facilityTypeFromDBAfterDelete.size());
-    }
 
 
-    private void setDatabaseConnection(IBeerDisitributionGameDAO gameDAO, DatabaseConnection connection) {
+    private void setDatabaseConnection(IBeerDisitributionGameDAO iBeerDisitributionGameDAO, DatabaseConnection connection) {
         try {
-            Field connField = gameDAO.getClass().getDeclaredField("databaseConnection");
+            Field connField = iBeerDisitributionGameDAO.getClass().getDeclaredField("databaseConnection");
             connField.setAccessible(true);
-            connField.set(gameDAO, connection);
+            connField.set(iBeerDisitributionGameDAO, connection);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             LOGGER.log(Level.SEVERE, e.toString(),e);
         }
