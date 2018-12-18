9780e68f447bb7dc968a8d50e89bb6506b1c10e7
Jelle Dimmendaal <jellecf.1@gmail.com>
2018-12-18 15:39:33 +0100

    Added methods for IWrapper

--- persistence/src/main/java/org/han/ica/asd/c/wrapper/IWrapper.java
+++ persistence/src/main/java/org/han/ica/asd/c/wrapper/IWrapper.java
@@ -1,5 +1,10 @@
 package org.han.ica.asd.c.wrapper;
 
-public interface IWrapper <T> {
-    private
+import org.han.ica.asd.c.model.dao_model.IDaoModel;
+import org.han.ica.asd.c.model.domain_objects.IDomainModel;
+
+public interface IWrapper <T > {
+    T startWrapping(T model);
+    T wrapToDaoModel(IDomainModel model);
+    T wrapToDomainModel(IDaoModel model);
 }
