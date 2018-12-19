8b8a66003126a7eacc647a02edba126ca82fa915
RickGZ <rickgz66@gmail.com>
2018-12-19 12:11:16 +0100

    added empty readfacility method

--- persistence/src/main/java/org/han/ica/asd/c/FacilityDAO.java
+++ persistence/src/main/java/org/han/ica/asd/c/FacilityDAO.java
@@ -1,6 +1,11 @@
 package org.han.ica.asd.c;
 
+import org.han.ica.asd.c.model.Facility;
+
 public class FacilityDAO {
 
 
+    public Facility readSpecificFacility(int facilityId, String gameId) {
+        return null;
+    }
 }
