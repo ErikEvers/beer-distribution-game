ac46229639f7c4d9042fed235f23b0ff08123a93
Bram Janssen <BM.Janssen1@student.han.nl>
2018-12-19 16:28:39 +0100

    Changed poms to work with moved FXMLloader

--- gui_program_agent/src/main/java/org/han/ica/asd/c/gui_program_agent/ProgramAgent.java
+++ gui_program_agent/src/main/java/org/han/ica/asd/c/gui_program_agent/ProgramAgent.java
@@ -1,28 +1,39 @@
 package org.han.ica.asd.c.gui_program_agent;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.NodeOrientation;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.han.ica.asd.c.fxml_helper.FXMLLoaderOnSteroids;
 
+import javax.inject.Inject;
+import javax.inject.Provider;
 import java.awt.*;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class Main extends Application {
+public class ProgramAgent {
     private static final String GAME_TITLE = "Beer Distribution Game";
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    private final Provider<FXMLLoaderOnSteroids> loaderProvider;
+
+    @Inject
+    public ProgramAgent(Provider<FXMLLoaderOnSteroids> loaderProvider) {
+        this.loaderProvider = loaderProvider;
+    }
+
+    public void setupScreen(Stage primaryStage) throws Exception {
         ResourceBundle resourceBundle = ResourceBundle.getBundle("languageResources");
 
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/ProgramAgent.fxml"), resourceBundle);
+        FXMLLoaderOnSteroids loader = loaderProvider.get();
+        loader.setResources(resourceBundle);
+        loader.setLocation(getClass().getResource("/fxml/ProgramAgentList.fxml"));
+        Parent root = loader.load();
 
         if (!ComponentOrientation.getOrientation(new Locale(System.getProperty("user.language"))).isLeftToRight()) {
             root.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
         }
+
         primaryStage.setTitle(GAME_TITLE);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
