53ebeb800b944f89917b2b012594196612bfbece
Chico Gerrits <CAGG.Gerrits@student.han.nl>
2018-12-19 16:02:22 +0100

    Added ui for join game

---
+++ gui_main_menu/src/main/java/org/han/ica/asd/c/gui_main_menu/JoinGameController.java
@@ -0,0 +1,4 @@
+package org.han.ica.asd.c.gui_main_menu;
+
+public class JoinGameController {
+}
--- gui_main_menu/src/main/java/org/han/ica/asd/c/gui_main_menu/MainMenu.java
+++ gui_main_menu/src/main/java/org/han/ica/asd/c/gui_main_menu/MainMenu.java
@@ -1,12 +1,15 @@
 package org.han.ica.asd.c.gui_main_menu;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.han.ica.asd.c.fxml_helper.FXMLLoaderOnSteroids;
 import javax.inject.Inject;
 import javax.inject.Provider;
 
-public class MainMenu {
+public class MainMenu extends Application {
 	private final Provider<FXMLLoaderOnSteroids> loaderProvider;
 
 	@Inject
@@ -14,11 +17,16 @@ public class MainMenu {
 		this.loaderProvider = loaderProvider;
 	}
 
-	public void setupScreen(Stage primaryStage) throws Exception {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 		FXMLLoaderOnSteroids loader = loaderProvider.get();
 		loader.setLocation(getClass().getResource("/fxml/MainMenu.fxml"));
-
 		primaryStage.setScene(new Scene(loader.load()));
 		primaryStage.show();
 	}
+
+	public static void main(String[] args){
+		launch(args);
+	}
+
 }
