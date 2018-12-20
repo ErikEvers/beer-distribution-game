fd59fe65ff41c4cadb0e5da81337b979a2b262fc
Jamiel van Engen <JL.vanEngen@student.han.nl>
2018-12-20 12:04:56 +0100

    Re-added the Persistence.java

---
+++ persistence/src/main/java/org/han/ica/asd/c/Persistence.java
@@ -0,0 +1,68 @@
+package org.han.ica.asd.c;
+
+
+import org.han.ica.asd.c.gamelogic.public_interfaces.IPersistence;
+import org.han.ica.asd.c.model.dao_model.BeerGame;
+import org.han.ica.asd.c.model.dao_model.FacilityLinkedTo;
+import org.han.ica.asd.c.model.dao_model.FacilityTurn;
+import org.han.ica.asd.c.model.dao_model.GameBusinessRulesInFacilityTurn;
+import org.han.ica.asd.c.model.dao_model.Round;
+
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+
+public class Persistence implements IPersistence {
+
+	@Inject
+	@Named("RoundDAO")
+	private IBeerDisitributionGameDAO roundDAO;
+
+	@Inject
+	@Named("BeergameDAO")
+	private IBeerDisitributionGameDAO beergameDAO;
+
+	@Inject
+	@Named("GameBusinessRulesRulesInFacilityTurnDAO")
+	private IBeerDisitributionGameDAO gameBusinessRulesInFacilityTurnDAO;
+
+	@Inject
+	@Named("FacilityTurnDAO")
+	private IBeerDisitributionGameDAO facilityTurnDAO;
+
+
+	public Persistence(){
+		//Empty constructor for GUICE
+	}
+
+	public void saveRoundData(Round rounddata)
+	{
+		((RoundDAO)roundDAO).createRound(rounddata.getGameId(), rounddata.getRoundId());
+	}
+
+	public RoundDB fetchRoundData(String gameId, int roundId)
+	{
+		return ((RoundDAO)roundDAO).getRound(gameId,roundId);
+	}
+
+	public BeerGameDB getGameLog(String gameId)
+	{
+		return ((BeergameDAO)beergameDAO).getGameLog(gameId);
+	}
+
+	public void logUsedBusinessRuleToCreateOrder(GameBusinessRulesInFacilityTurn gameBusinessRulesInFacilityTurn)
+	{
+		((GameBusinessRulesInFacilityTurnDAO)gameBusinessRulesInFacilityTurnDAO).createTurn(gameBusinessRulesInFacilityTurn);
+	}
+
+	public FacilityTurn fetchTurnData(Round round, FacilityLinkedTo facility)
+	{
+		return ((FacilityTurnDAO)facilityTurnDAO).fetchTurn(round,facility);
+	}
+
+	public void saveTurnData(FacilityTurn turn)
+	{
+		((FacilityTurnDAO)facilityTurnDAO).createTurn(turn);
+	}
+}
