11d4b075b24e1f05d981dec5837c73305d9d357a
RickGZ <rickgz66@gmail.com>
2018-12-20 16:48:04 +0100

    created player component class

---
+++ player/src/main/java/org/han/ica/asd/c/PlayerComponent.java
@@ -0,0 +1,39 @@
+package org.han.ica.asd.c;
+
+import org.han.ica.asd.c.model.domain_objects.Facility;
+import org.han.ica.asd.c.model.domain_objects.ProgrammedAgent;
+import org.han.ica.asd.c.public_interfaces.IPlayerComponent;
+
+public class PlayerComponent implements IPlayerComponent {
+
+
+    @Override
+    public void activatePlayer() {
+
+    }
+
+    @Override
+    public void activateGameAgent() {
+
+    }
+
+    @Override
+    public void seeOtherFacilities() {
+
+    }
+
+    @Override
+    public void placeOrder(int amount) {
+
+    }
+
+    @Override
+    public void selectProgrammedAgent(ProgrammedAgent programmedagent) {
+
+    }
+
+    @Override
+    public void chooseFacility(Facility facility) {
+
+    }
+}
