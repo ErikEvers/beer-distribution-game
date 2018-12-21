1d9ee469e6f0171092266580f64c527ce6db794a
Oscar Katsma <OT.Katsma@student.han.nl>
2018-12-21 09:06:52 +0100

    Fix major code smell (empty constructor)

--- communication/src/main/java/org/han/ica/asd/c/messagehandler/MessageProcessor.java
+++ communication/src/main/java/org/han/ica/asd/c/messagehandler/MessageProcessor.java
@@ -8,14 +8,7 @@ import org.han.ica.asd.c.messagehandler.messagetypes.WhoIsTheLeaderMessage;
 
 public class MessageProcessor {
 
-
-
-
-    public MessageProcessor() {
-    }
-
-
-    public NodeInfoList getNodeInfoListFromConnector(){
+    public NodeInfoList getNodeInfoListFromConnector() {
         Connector connector = Connector.getInstance();
         return connector.getIps();
     }
@@ -26,8 +19,7 @@ public class MessageProcessor {
         String leaderIp = nodeInfoList.getLeaderIp();
         if (leaderIp != null) {
             whoIsTheLeaderMessage.setResponse(leaderIp);
-        }
-        else{
+        } else {
             whoIsTheLeaderMessage.setResponse(new LeaderNotPresentException("I dont have a leader at this moment"));
         }
         return whoIsTheLeaderMessage;
