35fa1dbc96ea43e001a9e9b3e67d02d96dd1252a
Carsten Flokstra <C.Flokstra@student.han.nl>
2018-12-21 09:17:41 +0100

    Removed unused bindings

--- bootstrap/src/main/java/org/han/ica/asd/c/bootstrap/BootstrapModule.java
+++ bootstrap/src/main/java/org/han/ica/asd/c/bootstrap/BootstrapModule.java
@@ -1,14 +1,7 @@
 package org.han.ica.asd.c.bootstrap;
 
 import com.google.inject.name.Names;
-
 import org.han.ica.asd.c.businessrule.BusinessRuleHandler;
-import org.han.ica.asd.c.dao.BeergameDAO;
-import org.han.ica.asd.c.dao.FacilityTurnDAO;
-import org.han.ica.asd.c.dao.GameBusinessRulesInFacilityTurnDAO;
-import org.han.ica.asd.c.dao.RoundDAO;
-
-
 import org.han.ica.asd.c.dbconnection.DBConnection;
 import org.han.ica.asd.c.dbconnection.IDatabaseConnection;
 import org.han.ica.asd.c.fxml_helper.AbstractModuleExtension;
@@ -25,12 +18,7 @@ public class BootstrapModule extends AbstractModuleExtension {
 	protected void configure() {
 		bind(AbstractModuleExtension.class).to(BootstrapModule.class);
 		bind(IDatabaseConnection.class).to(DBConnection.class);
-		bind(RoundDAO.class).to(RoundDAO.class);
-		bind(BeergameDAO.class).to(BeergameDAO.class);
-		bind(GameBusinessRulesInFacilityTurnDAO.class).to(GameBusinessRulesInFacilityTurnDAO.class);
-		bind(FacilityTurnDAO.class).to(FacilityTurnDAO.class);
 		bind(IBusinessRules.class).to(BusinessRuleHandler.class);
-
 		bind(IGUIHandler.class).annotatedWith(Names.named("MainMenu")).to(MainMenu.class);
 		bind(IGUIHandler.class).annotatedWith(Names.named("ReplayGame")).to(ReplayGame.class);
 		bind(IGUIHandler.class).annotatedWith(Names.named("ProgramAgent")).to(ProgramAgent.class);
