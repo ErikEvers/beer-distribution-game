73dfa373dcd689af008dc4ac096607302bb10bb9
Yarno Boelens <yarno@print.com>
2018-12-24 18:54:52 +0100

    Created FXML with resources and very methods to eventually handle button clicks.

--- gui_spelers_beheren/src/main/java/org/han/ica/asd/c/gui_spelers_beheren/MonitorPlayers.java
+++ gui_spelers_beheren/src/main/java/org/han/ica/asd/c/gui_spelers_beheren/MonitorPlayers.java
@@ -3,8 +3,10 @@ package org.han.ica.asd.c.gui_spelers_beheren;
 import org.han.ica.asd.c.fxml_helper.FXMLLoaderOnSteroids;
 import org.han.ica.asd.c.fxml_helper.IGUIHandler;
 
+import java.util.ResourceBundle;
+
 public class MonitorPlayers implements IGUIHandler {
 	public void setupScreen() {
-		MonitorPlayersScreenController controller = FXMLLoaderOnSteroids.getScreen(null, getClass().getResource("/fxml/MonitorPlayersScreen.fxml"));
+		MonitorPlayersScreenController controller = FXMLLoaderOnSteroids.getScreen(ResourceBundle.getBundle("languageResources"), getClass().getResource("/fxml/MonitorPlayersScreen.fxml"));
 	}
 }
--- gui_spelers_beheren/src/main/java/org/han/ica/asd/c/gui_spelers_beheren/MonitorPlayersScreenController.java
+++ gui_spelers_beheren/src/main/java/org/han/ica/asd/c/gui_spelers_beheren/MonitorPlayersScreenController.java
@@ -1,4 +1,16 @@
 package org.han.ica.asd.c.gui_spelers_beheren;
 
 public class MonitorPlayersScreenController {
+
+	public void handleReturnToGameRoomButtonClick() {
+
+	}
+
+	public void handleStartGameButtonClick() {
+
+	}
+
+	public void handleKickPlayerButtonClick() {
+
+	}
 }
