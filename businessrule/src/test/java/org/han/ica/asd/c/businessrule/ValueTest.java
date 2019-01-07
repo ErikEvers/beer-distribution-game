b569e964cbc6cf231e37e2a345af259b89ea0122
Sven Timmers <S.Timmers@student.han.nl>
2019-01-07 10:59:12 +0100

    Added tests for AST classes

--- businessrule/src/test/java/org/han/ica/asd/c/businessrule/ValueTest.java
+++ businessrule/src/test/java/org/han/ica/asd/c/businessrule/ValueTest.java
@@ -1,4 +1,76 @@
 package org.han.ica.asd.c.businessrule;
 
+import org.han.ica.asd.c.businessrule.parser.ast.action.Person;
+import org.han.ica.asd.c.businessrule.parser.ast.operations.Value;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.TestCase.*;
+import static junit.framework.TestCase.assertFalse;
+
 public class ValueTest {
+    private Value value = new Value();
+
+    @BeforeEach
+    void before(){
+        value.addValue("1");
+    }
+
+    @Test
+    void testPerson_hashCode_True() {
+        Value testValue1 = new Value();
+        Value testValue2 = new Value();
+
+        assertEquals(testValue1.hashCode(), testValue2.hashCode());
+    }
+
+    @Test
+    void testPerson_Equal_True() {
+        Value valueTest = new Value();
+        valueTest.addValue("1");
+        boolean match = value.equals(valueTest);
+        assertTrue(match);
+    }
+
+    @Test
+    void testPerson_Equal_True2() {
+        boolean match = value.equals(value);
+        assertTrue(match);
+    }
+
+    @Test
+    void testPerson_Equal_False() {
+        Value valueTest = new Value();
+        valueTest.addValue("2");
+        boolean match = value.equals(valueTest);
+        assertFalse(match);
+    }
+
+    @Test
+    void testPerson_Equal_FalseNull() {
+        boolean match = value.equals(null);
+        assertFalse(match);
+    }
+
+    @Test
+    void testPerson_AddValue_Lowest(){
+        Value testValue = new Value();
+        testValue.addValue("smallest");
+
+        String exp = "lowest";
+        String cur = testValue.getValue();
+
+        assertEquals(exp,cur);
+    }
+
+    @Test
+    void testPerson_AddValue_Highest(){
+        Value testValue = new Value();
+        testValue.addValue("biggest");
+
+        String exp = "highest";
+        String cur = testValue.getValue();
+
+        assertEquals(exp,cur);
+    }
 }
