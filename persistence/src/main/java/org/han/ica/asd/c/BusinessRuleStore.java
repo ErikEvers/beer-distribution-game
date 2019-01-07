138b2f80d3a3c5d97bb39ad6ad33518a01f429cf
Marnix Kortsilius <MD.Kortsilius@student.han.nl>
2019-01-07 16:51:02 +0100

    Added the implementation for the new methods given in the IBusinessRuleStore.

--- persistence/src/main/java/org/han/ica/asd/c/BusinessRuleStore.java
+++ persistence/src/main/java/org/han/ica/asd/c/BusinessRuleStore.java
@@ -1,7 +1,9 @@
 package org.han.ica.asd.c;
 
+import org.han.ica.asd.c.dao.ProgrammedAgentDAO;
 import org.han.ica.asd.c.dao.ProgrammedBusinessRulesDAO;
 import org.han.ica.asd.c.interfaces.businessrule.IBusinessRuleStore;
+import org.han.ica.asd.c.model.domain_objects.ProgrammedAgent;
 import org.han.ica.asd.c.model.domain_objects.ProgrammedBusinessRules;
 
 import javax.inject.Inject;
@@ -14,6 +16,9 @@ public class BusinessRuleStore implements IBusinessRuleStore {
 
     @Inject
     ProgrammedBusinessRulesDAO programmedBusinessRulesDao;
+
+    @Inject
+    ProgrammedAgentDAO programmedAgentDAO;
 
     @Override
     public List<String> readInputBusinessRules(String agentName) {
@@ -34,5 +39,23 @@ public class BusinessRuleStore implements IBusinessRuleStore {
             programmedBusinessRule.setProgrammedAST(businessRule.getValue());
             programmedBusinessRulesDao.createProgrammedbusinessRule(programmedBusinessRule, agentName);
         }
+    }
+
+    @Override
+    public List<String> getAllProgrammedAgents() {
+        List<String> programmedAgentNames = null;
+
+        List<ProgrammedAgent> dbProgrammedAgents = programmedAgentDAO.readAllProgrammedAgents();
+
+        for (ProgrammedAgent programmedAgent : dbProgrammedAgents){
+            programmedAgentNames.add(programmedAgent.getProgrammedAgentName());
+        }
+        return programmedAgentNames;
+    }
+
+    @Override
+    public void deleteProgrammedAgent(String agentName) {
+        ProgrammedAgent programmedAgent = new ProgrammedAgent(agentName, null);
+        programmedAgentDAO.deleteProgrammedAgent(programmedAgent);
     }
 }
