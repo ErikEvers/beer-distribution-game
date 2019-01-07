e48f53e3ce3c31bddf0e2f393cbf20cefa2f59ad
Mathijs Bouwmeister <MR.Bouwmeister@student.han.nl>
2019-01-07 17:15:52 +0100

    code verbeterd

--- communication/src/main/java/org/han/ica/asd/c/messagehandler/sending/SendInTransaction.java
+++ communication/src/main/java/org/han/ica/asd/c/messagehandler/sending/SendInTransaction.java
@@ -28,89 +28,23 @@ public class SendInTransaction {
         //TODO implement with this: https://stackoverflow.com/questions/9148899/returning-value-from-thread
 
         transactionMessage.setPhaseToStage();
-
-        try {
-            /*for (String ip : ips) {
-                sendCanCommit(ip);
-            }*/
-            sendCanCommit(socketClient.sendToAll(ips, transactionMessage));
-        } catch (Exception ex) {
-            //rollback
-            LOGGER.log(Level.INFO, "Stagecommit failed, rolling back");
-            transactionMessage.setPhaseToRollback();
-            socketClient.sendToAll(ips, transactionMessage);
-        }
+        handleStagePhase(socketClient.sendToAll(ips, transactionMessage));
     }
 
-    private void sendCanCommit(Map<String, Object> map) {
+    private void handleStagePhase(Map<String, Object> map) {
         boolean allSuccessful = true;
         for(Object value : map.values()) {
             if(value instanceof Exception)
                 allSuccessful = false;
         }
 
-        if(allSuccessful)
+        if(allSuccessful) {
+            transactionMessage.setPhaseToCommit();
             socketClient.sendToAll(ips, transactionMessage);
+        }
         else {
             transactionMessage.setPhaseToRollback();
             socketClient.sendToAll(ips, transactionMessage);
         }
     }
-
-    //
-//    /**
-//     * Sends a commit message to an ip.
-//     * @param ip
-//     */
-//    private void sendCanCommit(String ip) {
-//        Thread t = new Thread(() -> {
-//            try {
-//                ResponseMessage response = (ResponseMessage) socketClient.sendObjectWithResponse(ip, transactionMessage);
-//                canCommitFinished(response.getIsSuccess());
-//
-//            } catch (IOException | ClassNotFoundException e) {
-//                LOGGER.log(Level.SEVERE, e.getMessage(), e);
-//            }
-//        });
-//        t.setDaemon(true);
-//        t.start();
-//    }
-//
-//    /**
-//     * Checks if all commits are successful, if so then a commit is staged. If not, then a rollback is staged.
-//     * @param isSuccess
-//     */
-//    private void canCommitFinished(boolean isSuccess) {
-//        numberFinished++;
-//        if (isSuccess) {
-//            numberOfSuccesses++;
-//        }
-//        if (numberOfSuccesses == numberOfThreads) {
-//            //All succeeded, do commit.
-//            transactionMessage.setPhaseToCommit();
-//            send();
-//        } else if (numberFinished == numberOfThreads) {
-//            //Failure, rollback
-//            transactionMessage.setPhaseToRollback();
-//            send();
-//        }
-//    }
-//
-//    /**
-//     * Send an object to every ip
-//     */
-//    private void send() {
-//        for (String ip : ips) {
-//            Thread t = new Thread(() -> {
-//                try {
-//                    ResponseMessage response = (ResponseMessage) socketClient.sendObjectWithResponse(ip, transactionMessage);
-//
-//                } catch (IOException | ClassNotFoundException e) {
-//                    LOGGER.log(Level.SEVERE, e.getMessage(), e);
-//                }
-//            });
-//            t.setDaemon(true);
-//            t.start();
-//        }
-//    }
 }
