92108ed2beeaea639b2d1f56f5707c6caa92f7eb
Rogier Grobbee <r.grobbee@gmail.com>
2019-01-07 13:50:21 +0100

    Added javadoc to turnmodelmessage

--- communication/src/main/java/org/han/ica/asd/c/messagehandler/messagetypes/TurnModelMessage.java
+++ communication/src/main/java/org/han/ica/asd/c/messagehandler/messagetypes/TurnModelMessage.java
@@ -17,12 +17,28 @@ public class TurnModelMessage extends GameMessage implements Serializable {
         this.turnModel = roundData;
     }
 
+    /**
+     * Use this to create a serverResponse to a TurnModelMessage
+     * If a TurnModelMessage is send via the sendObjectWithResponseGeneric method, it expects a response of the same object type.
+     * This method is used to create that response.
+     * @author Rogier
+     * @param isSuccess
+     * @return
+     */
     public static TurnModelMessage createResponseMessage(boolean isSuccess){
         TurnModelMessage turnModelMessage = new TurnModelMessage(null);
         turnModelMessage.isSuccess = isSuccess;
         return turnModelMessage;
     }
 
+    /**
+     * Use this to create a serverResponse to a TurnModelMessage
+     * If a TurnModelMessage is send via the sendObjectWithResponseGeneric method, it expects a response of the same object type.
+     * This method is used to create that response.
+     * @author Rogier
+     * @param exception
+     * @return
+     */
     public static TurnModelMessage createResponseMessage(Exception exception){
         TurnModelMessage turnModelMessage = new TurnModelMessage(null);
         turnModelMessage.setException(exception);
