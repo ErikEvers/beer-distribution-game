031669ebacfbf7b3e881fedc316b899bc340de4d
Marnix Kortsilius <MD.Kortsilius@student.han.nl>
2019-01-08 14:02:09 +0100

    Removed star import.

--- persistence/src/test/java/org/han/ica/asd/c/BusinessRuleStoreTest.java
+++ persistence/src/test/java/org/han/ica/asd/c/BusinessRuleStoreTest.java
@@ -24,7 +24,10 @@ import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class BusinessRuleStoreTest {
 
