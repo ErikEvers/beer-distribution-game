4ce28e2c46f5c6567a985000e1c032ce12ed11b3
Erik Evers <E.Evers1@student.han.nl>
2019-01-08 15:22:08 +0100

    Changed imports

--- agent/src/main/java/org/han/ica/asd/c/agent/Agent.java
+++ agent/src/main/java/org/han/ica/asd/c/agent/Agent.java
@@ -3,7 +3,13 @@ package org.han.ica.asd.c.agent;
 import org.han.ica.asd.c.interfaces.businessrule.IBusinessRules;
 import org.han.ica.asd.c.interfaces.gameleader.IPersistence;
 import org.han.ica.asd.c.interfaces.gamelogic.IParticipant;
-import org.han.ica.asd.c.model.domain_objects.*;
+import org.han.ica.asd.c.model.domain_objects.Configuration;
+import org.han.ica.asd.c.model.domain_objects.Facility;
+import org.han.ica.asd.c.model.domain_objects.GameAgent;
+import org.han.ica.asd.c.model.domain_objects.GameBusinessRules;
+import org.han.ica.asd.c.model.domain_objects.GameBusinessRulesInFacilityTurn;
+import org.han.ica.asd.c.model.domain_objects.GameRoundAction;
+import org.han.ica.asd.c.model.domain_objects.Round;
 import org.han.ica.asd.c.model.interface_models.ActionModel;
 
 import javax.inject.Inject;
@@ -38,50 +44,50 @@ public class Agent extends GameAgent implements IParticipant {
     /**
      * Generates actions of an agent using the defined business rules.
      *
-     * @param round         The round data of last round used to determine what actions the agent is going to do.
-     * @return              A GameRoundAction with all actions that the agent wants to do.
+     * @param round The round data of last round used to determine what actions the agent is going to do.
+     * @return A GameRoundAction with all actions that the agent wants to do.
      */
     private GameRoundAction generateRoundActions(Round round) {
-        Map<Facility, Integer>  targetOrderMap          = new HashMap<>();
-        Map<Facility, Integer>  targetDeliverMap        = new HashMap<>();
-        List<GameBusinessRules> triggeredBusinessRules  = new ArrayList<>();
+        Map<Facility, Integer> targetOrderMap = new HashMap<>();
+        Map<Facility, Integer> targetDeliverMap = new HashMap<>();
+        List<GameBusinessRules> triggeredBusinessRules = new ArrayList<>();
 
         for (GameBusinessRules gameBusinessRules : this.getGameBusinessRules()) {
             ActionModel actionModel = businessRules.evaluateBusinessRule(gameBusinessRules.getGameAST(), round);
-            if(actionModel != null) {
-								if (targetOrderMap.isEmpty() && actionModel.isOrderType()) {
-										Facility targetFacility = this.resolveLowerFacilityId(actionModel.facilityId);
-										if(targetFacility == null) {
-											break;
-										}
-										targetOrderMap.put(targetFacility, actionModel.amount);
-										triggeredBusinessRules.add(gameBusinessRules);
-								} else if (targetDeliverMap.isEmpty() && actionModel.isDeliverType()) {
-										Facility targetFacility = this.resolveHigherFacilityId(actionModel.facilityId);
-										if(targetFacility == null) {
-											break;
-										}
-										targetDeliverMap.put(targetFacility, actionModel.amount);
-										triggeredBusinessRules.add(gameBusinessRules);
-								} else if (!targetOrderMap.isEmpty() && !targetDeliverMap.isEmpty()) {
-										break;
-								}
+            if (actionModel != null) {
+                if (targetOrderMap.isEmpty() && actionModel.isOrderType()) {
+                    Facility targetFacility = this.resolveLowerFacilityId(actionModel.facilityId);
+                    if (targetFacility == null) {
+                        break;
+                    }
+                    targetOrderMap.put(targetFacility, actionModel.amount);
+                    triggeredBusinessRules.add(gameBusinessRules);
+                } else if (targetDeliverMap.isEmpty() && actionModel.isDeliverType()) {
+                    Facility targetFacility = this.resolveHigherFacilityId(actionModel.facilityId);
+                    if (targetFacility == null) {
+                        break;
+                    }
+                    targetDeliverMap.put(targetFacility, actionModel.amount);
+                    triggeredBusinessRules.add(gameBusinessRules);
+                } else if (!targetOrderMap.isEmpty() && !targetDeliverMap.isEmpty()) {
+                    break;
+                }
             }
         }
 
         persistence.logUsedBusinessRuleToCreateOrder(
-            new GameBusinessRulesInFacilityTurn(getFacility().getFacilityId(), round.getRoundId(), getGameAgentName() + 1, triggeredBusinessRules));
+                new GameBusinessRulesInFacilityTurn(getFacility().getFacilityId(), round.getRoundId(), getGameAgentName() + 1, triggeredBusinessRules));
         return new GameRoundAction(targetOrderMap, targetDeliverMap);
     }
 
     /**
      * Returns the facility of the identifying integer. When the facility is not found, it'll return NULL.
      *
-     * @param targetFacilityId  The identifying integer of the facility that needs to be resolved
-     * @return                  The facility below the current facility that needs to be resolved. NULL when facility is not found.
+     * @param targetFacilityId The identifying integer of the facility that needs to be resolved
+     * @return The facility below the current facility that needs to be resolved. NULL when facility is not found.
      */
     private Facility resolveLowerFacilityId(int targetFacilityId) {
-				List<Facility> links = new ArrayList<>(configuration.getFacilitiesLinkedTo().get(getFacility()));
+        List<Facility> links = new ArrayList<>(configuration.getFacilitiesLinkedTo().get(getFacility()));
         for (Facility link : links) {
             if (targetFacilityId == link.getFacilityId()) {
                 return link;
@@ -90,28 +96,28 @@ public class Agent extends GameAgent implements IParticipant {
         return null;
     }
 
-	/**
-	 * Returns the facility of the identifying integer. When the facility is not found, it'll return NULL.
-	 *
-	 * @param targetFacilityId  The identifying integer of the facility that needs to be resolved
-	 * @return                  The facility above the current facility that needs to be resolved. NULL when facility is not found.
-	 */
-	private Facility resolveHigherFacilityId(int targetFacilityId) {
-		for(Map.Entry<Facility, List<Facility>> link: configuration.getFacilitiesLinkedTo().entrySet()) {
-			if(link.getValue().stream().anyMatch(f -> f.getFacilityId() == getFacility().getFacilityId()) && link.getKey().getFacilityId() == targetFacilityId) {
-				return link.getKey();
-			}
-		}
-		return null;
-	}
+    /**
+     * Returns the facility of the identifying integer. When the facility is not found, it'll return NULL.
+     *
+     * @param targetFacilityId The identifying integer of the facility that needs to be resolved
+     * @return The facility above the current facility that needs to be resolved. NULL when facility is not found.
+     */
+    private Facility resolveHigherFacilityId(int targetFacilityId) {
+        for (Map.Entry<Facility, List<Facility>> link : configuration.getFacilitiesLinkedTo().entrySet()) {
+            if (link.getValue().stream().anyMatch(f -> f.getFacilityId() == getFacility().getFacilityId()) && link.getKey().getFacilityId() == targetFacilityId) {
+                return link.getKey();
+            }
+        }
+        return null;
+    }
 
-	@Override
-	public GameRoundAction executeTurn(Round round) {
-		return generateRoundActions(round);
-	}
+    @Override
+    public GameRoundAction executeTurn(Round round) {
+        return generateRoundActions(round);
+    }
 
-	@Override
-	public Facility getParticipant() {
-		return getFacility();
-	}
+    @Override
+    public Facility getParticipant() {
+        return getFacility();
+    }
 }
--- businessrule/src/main/java/org/han/ica/asd/c/businessrule/engine/BusinessRuleFactory.java
+++ businessrule/src/main/java/org/han/ica/asd/c/businessrule/engine/BusinessRuleFactory.java
@@ -1,31 +1,38 @@
 package org.han.ica.asd.c.businessrule.engine;
 
 import org.han.ica.asd.c.businessrule.parser.ast.ASTNode;
+import org.han.ica.asd.c.businessrule.parser.ast.Default;
 import org.han.ica.asd.c.businessrule.parser.ast.action.Action;
 import org.han.ica.asd.c.businessrule.parser.ast.action.ActionReference;
-import org.han.ica.asd.c.businessrule.parser.ast.Default;
 import org.han.ica.asd.c.businessrule.parser.ast.action.Person;
 import org.han.ica.asd.c.businessrule.parser.ast.comparison.Comparison;
 import org.han.ica.asd.c.businessrule.parser.ast.comparison.ComparisonStatement;
 import org.han.ica.asd.c.businessrule.parser.ast.comparison.ComparisonValue;
-import org.han.ica.asd.c.businessrule.parser.ast.operations.*;
+import org.han.ica.asd.c.businessrule.parser.ast.operations.AddOperation;
+import org.han.ica.asd.c.businessrule.parser.ast.operations.DivideOperation;
+import org.han.ica.asd.c.businessrule.parser.ast.operations.MultiplyOperation;
+import org.han.ica.asd.c.businessrule.parser.ast.operations.SubtractOperation;
+import org.han.ica.asd.c.businessrule.parser.ast.operations.Value;
 import org.han.ica.asd.c.businessrule.parser.ast.operators.BooleanOperator;
 import org.han.ica.asd.c.businessrule.parser.ast.operators.CalculationOperator;
 import org.han.ica.asd.c.businessrule.parser.ast.operators.ComparisonOperator;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class BusinessRuleFactory {
-	private static final Logger LOGGER = Logger.getLogger(Logger.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(Logger.class.getName());
     private static final Map<String, Class<? extends ASTNode>> ASTNODE_CLASS_MAP;
+
     static {
         Map<String, Class<? extends ASTNode>> astNodeClassMap = new TreeMap<>();
         astNodeClassMap.put("A", Action.class);
         astNodeClassMap.put("C", Comparison.class);
         astNodeClassMap.put("D", Default.class);
-	    astNodeClassMap.put("P", Person.class);
+        astNodeClassMap.put("P", Person.class);
         astNodeClassMap.put("V", Value.class);
         astNodeClassMap.put("AR", ActionReference.class);
         astNodeClassMap.put("CV", ComparisonValue.class);
@@ -40,19 +47,20 @@ class BusinessRuleFactory {
         ASTNODE_CLASS_MAP = Collections.unmodifiableMap(astNodeClassMap);
     }
 
-	/**
-	 * Takes the identifier of the business rule script and transforms it into the object of the identifier
-	 * @param identifier    The identifier of the business rule script
-	 * @return              The object which the identifier represents
-	 */
-	ASTNode create(String identifier) {
-	    if(ASTNODE_CLASS_MAP.containsKey(identifier)) {
+    /**
+     * Takes the identifier of the business rule script and transforms it into the object of the identifier
+     *
+     * @param identifier The identifier of the business rule script
+     * @return The object which the identifier represents
+     */
+    ASTNode create(String identifier) {
+        if (ASTNODE_CLASS_MAP.containsKey(identifier)) {
             try {
                 return ASTNODE_CLASS_MAP.get(identifier).newInstance();
             } catch (Exception e) {
-	            LOGGER.log(Level.SEVERE, e.toString(), e);
+                LOGGER.log(Level.SEVERE, e.toString(), e);
             }
         }
-	    return null;
+        return null;
     }
 }
