da3f41106385de17f377b68acce57a3c5de8c907
RickGZ <rickgz66@gmail.com>
2019-01-08 15:28:14 +0100

    added getAllGames method to comm interface

--- interfaces/src/main/java/org/han/ica/asd/c/interfaces/gamelogic/IConnectedForPlayer.java
+++ interfaces/src/main/java/org/han/ica/asd/c/interfaces/gamelogic/IConnectedForPlayer.java
@@ -3,7 +3,10 @@ package org.han.ica.asd.c.interfaces.gamelogic;
 import org.han.ica.asd.c.model.domain_objects.Round;
 import org.han.ica.asd.c.interfaces.communication.IConnectorObserver;
 
+import java.util.List;
+
 public interface IConnectedForPlayer {
     void sendTurnData(Round turn);
     void addObserver(IConnectorObserver observer);
+    List<String> getAllGames();
 }
