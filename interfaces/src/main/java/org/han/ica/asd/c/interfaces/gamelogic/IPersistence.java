fda5e375952afcef777b51f55b24f191443678bf
RickGZ <rickgz66@gmail.com>
2019-01-08 18:02:10 +0100

    created method in ipersistence to get saved agents from database

--- interfaces/src/main/java/org/han/ica/asd/c/interfaces/gamelogic/IPersistence.java
+++ interfaces/src/main/java/org/han/ica/asd/c/interfaces/gamelogic/IPersistence.java
@@ -1,11 +1,13 @@
 package org.han.ica.asd.c.interfaces.gamelogic;
 
+import org.han.ica.asd.c.agent.Agent;
 import org.han.ica.asd.c.model.domain_objects.BeerGame;
 import org.han.ica.asd.c.model.domain_objects.Facility;
 import org.han.ica.asd.c.model.domain_objects.GameBusinessRulesInFacilityTurn;
 import org.han.ica.asd.c.model.domain_objects.Player;
 import org.han.ica.asd.c.model.domain_objects.Round;
 
+import java.util.List;
 import java.util.Map;
 
 public interface IPersistence {
@@ -16,4 +18,5 @@ public interface IPersistence {
     BeerGame getGameLog(String gameId);
     Player getPlayerById(String playerId);
     void logUsedBusinessRuleToCreateOrder(GameBusinessRulesInFacilityTurn gameBusinessRulesInFacilityTurn);
+    List<Agent> getSavedAgents();
 }
