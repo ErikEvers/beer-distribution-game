f00e110968355269ed7b7e67fb979c7da5a33dd9
Wesley van Bergen <wesleyvbergen1996@gmail.com>
2019-01-08 15:01:25 +0100

    added connector hf

--- communication/src/main/java/org/han/ica/asd/c/Connector.java
+++ communication/src/main/java/org/han/ica/asd/c/Connector.java
@@ -4,21 +4,26 @@ import org.han.ica.asd.c.discovery.DiscoveryException;
 import org.han.ica.asd.c.discovery.IFinder;
 import org.han.ica.asd.c.discovery.RoomException;
 import org.han.ica.asd.c.discovery.RoomFinder;
+import org.han.ica.asd.c.model.domain_objects.Facility;
+import org.han.ica.asd.c.model.domain_objects.RoomModel;
 import org.han.ica.asd.c.faultdetection.FaultDetectionClient;
 import org.han.ica.asd.c.faultdetection.FaultDetector;
 import org.han.ica.asd.c.faultdetection.exceptions.NodeCantBeReachedException;
 import org.han.ica.asd.c.faultdetection.nodeinfolist.NodeInfo;
 import org.han.ica.asd.c.faultdetection.nodeinfolist.NodeInfoList;
+import org.han.ica.asd.c.interfaces.communication.IConnecterForSetup;
 import org.han.ica.asd.c.interfaces.communication.IConnectorObserver;
-import org.han.ica.asd.c.interfaces.gui_join_game.IConnecterForSetup;
 import org.han.ica.asd.c.messagehandler.receiving.GameMessageReceiver;
 import org.han.ica.asd.c.messagehandler.sending.GameMessageClient;
-
 import org.han.ica.asd.c.model.domain_objects.Configuration;
-import org.han.ica.asd.c.model.domain_objects.RoomModel;
 import org.han.ica.asd.c.model.domain_objects.Round;
 import org.han.ica.asd.c.socketrpc.SocketServer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -26,12 +31,13 @@ import java.util.logging.Logger;
 
 
 public class Connector implements IConnecterForSetup {
-private static Connector instance = null;
+    private static Connector instance = null;
     private ArrayList<IConnectorObserver> observers;
     private NodeInfoList nodeInfoList;
     private FaultDetector faultDetector;
     private IFinder finder;
     private GameMessageClient gameMessageClient;
+    private String externalIP;
 
     private static final Logger LOGGER = Logger.getLogger(Connector.class.getName());
 
@@ -41,6 +47,7 @@ private static Connector instance = null;
         finder = new RoomFinder();
         gameMessageClient = new GameMessageClient();
         faultDetector = new FaultDetector(observers);
+        externalIP = getExternalIP();
 
         SocketServer socketServer = new SocketServer(new MessageDirector(new GameMessageReceiver(observers), faultDetector.getFaultDetectionMessageReceiver()));
         socketServer.startThread();
@@ -75,10 +82,10 @@ private static Connector instance = null;
         return new ArrayList<>();
     }
 
-    public RoomModel createRoom(String roomName, String ip, String password){
+    public RoomModel createRoom(String roomName, String password){
         try {
-            RoomModel createdRoom = finder.createGameRoomModel(roomName, ip, password);
-            nodeInfoList.add(new NodeInfo(ip, true, true));
+            RoomModel createdRoom = finder.createGameRoomModel(roomName, externalIP, password);
+            nodeInfoList.add(new NodeInfo(externalIP, true, true));
             return createdRoom;
         } catch (DiscoveryException e) {
             LOGGER.log(Level.INFO, e.getMessage(), e);
@@ -86,9 +93,9 @@ private static Connector instance = null;
         return null;
     }
 
-    public RoomModel joinRoom(String roomName, String ip, String password){
+    public RoomModel joinRoom(String roomName, String password){
         try {
-            RoomModel joinedRoom = finder.joinGameRoomModel(roomName, ip, password);
+            RoomModel joinedRoom = finder.joinGameRoomModel(roomName, externalIP, password);
             if(makeConnection(joinedRoom.getLeaderIP())){
                 addLeaderToNodeInfoList(joinedRoom.getLeaderIP());
                 setJoiner();
@@ -120,6 +127,22 @@ private static Connector instance = null;
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
         }
     }
+    public void removeHostFromRoom(RoomModel room, String hostIP){
+        try {
+            finder.removeHostFromRoom(room, hostIP);
+        } catch (DiscoveryException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void removeYourselfFromRoom(RoomModel room) {
+        try {
+            finder.removeHostFromRoom(room, externalIP);
+        } catch (DiscoveryException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
 
     public void addObserver(IConnectorObserver observer) {
         observers.add(observer);
@@ -146,7 +169,7 @@ private static Connector instance = null;
     public void addToNodeInfoList(String txtIP) {
         nodeInfoList.addIp(txtIP);
     }
-    
+
     public void addLeaderToNodeInfoList(String txtIp){
         NodeInfo nodeInfo = new NodeInfo();
         nodeInfo.setLeader(true);
@@ -175,5 +198,29 @@ private static Connector instance = null;
 
     public NodeInfoList getIps() {
         return nodeInfoList;
+    }
+
+    public String getExternalIP(){
+        URL whatismyip = null;
+        try {
+            whatismyip = new URL("http://checkip.amazonaws.com");
+        } catch (MalformedURLException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        }
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(
+                    whatismyip.openStream()));
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        }
+        String ip = null;
+        try {
+            ip = in.readLine();
+            in.close();
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        }
+        return ip;
     }
 }
