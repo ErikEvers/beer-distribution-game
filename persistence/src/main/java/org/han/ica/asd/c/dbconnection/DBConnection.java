8a5c6b77b36fb26c2550c90dd67ce4170396a663
Bram Janssen <BM.Janssen1@student.han.nl>
2019-01-08 17:54:57 +0100

    Merge branch 'feature/SIXGJM-499-main-application-flow-implementeren' of C:\Users\Bram\Documents\HAN - ICT\ICT Jaar 3\Blok 2 jaar 3\Werk\beer-distribution-game with conflicts.

--- persistence/src/main/java/org/han/ica/asd/c/dbconnection/DBConnection.java
+++ persistence/src/main/java/org/han/ica/asd/c/dbconnection/DBConnection.java
@@ -17,20 +17,16 @@ import java.util.logging.Logger;
 @Singleton
 public class DBConnection implements IDatabaseConnection {
 	private static final Path currentDir = Paths.get("");
-	private static final String CONNECTIONSTRING = "jdbc:sqlite:"+currentDir.toAbsolutePath().toString()+File.separator+"persistence"+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator;
+	private static final String CONNECTIONSTRING = "jdbc:sqlite:"+currentDir.toAbsolutePath().toString()+File.separator+"persistence"+ File.separator+"src"+ File.separator+"main"+File.separator+"resources"+File.separator;
 	private static final String DATABASENAME = "BeerGameDB.db";
 	private static final Logger LOGGER = Logger.getLogger(org.han.ica.asd.c.dbconnection.DBConnection.class.getName());
 
 	public DBConnection() {
-		createNewDatabase();
+		this.createNewDatabase();
 	}
 
-
 	public void createNewDatabase() {
-		File file = new File(CONNECTIONSTRING+DATABASENAME);
-		if(!file.exists()) {
-			runSQLScript("ddl.sql");
-		}
+		runSQLScript("ddl.sql");
 	}
 
 	public void runSQLScript(String scriptname) {
