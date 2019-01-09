00713cf14d99cd6f8515a5891fc504c0a1a037a3
Marnix Kortsilius <MD.Kortsilius@student.han.nl>
2019-01-09 22:18:44 +0100

    Added the screen and an empty controller to match the screen.

---
+++ gui_replay_game/src/main/java/org/han/ica/asd/c/gui_replay_game/ReplayGameRoundScreenController.java
@@ -0,0 +1,65 @@
+package org.han.ica.asd.c.gui_replay_game;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class ReplayGameRoundScreenController {
+
+    @FXML
+    private AnchorPane mainContainer;
+
+    @FXML
+    private AnchorPane facilitiesContainer;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    private Button previousRoundButton;
+
+    @FXML
+    private TextField roundId;
+
+    @FXML
+    private Label maxRoundId;
+
+    @FXML
+    private Button NextRoundButton;
+
+    @FXML
+    private Label gameName;
+
+    @FXML
+    private Button showGraph;
+
+    @FXML
+    void handleNextRoundButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handlePreviousRoundButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleReturnButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRoundIdChange(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void showGraphHandler(ActionEvent event) {
+
+    }
+
+}
