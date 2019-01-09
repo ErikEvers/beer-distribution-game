3a4a09e20ca64b85f284af7e4a0c063792988fe1
kaspervdw <kaspervdw@live.nl>
2019-01-09 11:04:01 +0100

    Added back to menu button functionality

---
+++ gui_manage_players/src/main/java/org/han/ica/asd/c/gui_manage_players/ManagePlayers.java
@@ -0,0 +1,19 @@
+package org.han.ica.asd.c.gui_manage_players;
+
+import org.han.ica.asd.c.fxml_helper.FXMLLoaderOnSteroids;
+import org.han.ica.asd.c.fxml_helper.IGUIHandler;
+
+import java.util.ResourceBundle;
+
+public class ManagePlayers implements IGUIHandler {
+
+
+	@Override
+	public void setData(Object[] data) {
+		// implement interface
+	}
+
+	public void setupScreen() {
+		FXMLLoaderOnSteroids.getScreen(ResourceBundle.getBundle("languageResources"), getClass().getResource("/fxml/ManagePlayersScreen.fxml"));
+	}
+}
