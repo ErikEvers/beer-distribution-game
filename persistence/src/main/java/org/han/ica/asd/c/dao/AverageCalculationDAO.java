f94bb24a8f02249b2fbae5a44e905c1eb7802a2e
Marnix Kortsilius <MD.Kortsilius@student.han.nl>
2019-01-09 10:48:19 +0100

    Refactored the DAO so it will return the required values and made the first test.

--- persistence/src/main/java/org/han/ica/asd/c/dao/AverageCalculationDAO.java
+++ persistence/src/main/java/org/han/ica/asd/c/dao/AverageCalculationDAO.java
@@ -2,11 +2,17 @@ package org.han.ica.asd.c.dao;
 
 import org.han.ica.asd.c.dbconnection.IDatabaseConnection;
 import org.han.ica.asd.c.exception.GameIdNotSetException;
+import org.han.ica.asd.c.model.domain_objects.FacilityTurn;
+import org.han.ica.asd.c.model.domain_objects.FacilityTurnDeliver;
+import org.han.ica.asd.c.model.domain_objects.FacilityTurnOrder;
 
 import javax.inject.Inject;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,8 +20,9 @@ public class AverageCalculationDAO {
     private static final Logger LOGGER = Logger.getLogger(AverageCalculationDAO.class.getName());
     private static final String READ_ALL_FACILITIES_WITH_FACILITYTYPE = "SELECT FacilityId FROM Facility WHERE GameId = ? AND FacilityName = ?;";
     private static final String READ_FACILITYTURN_FOR_FACILITY = "SELECT * FROM FacilityTurn WHERE GameId = ? AND RoundId = ? AND FacilityId = ?;";
-    private static final String READ_FACILITYTURNORDER_FOR_FACILITY = "SELECT OrderAmount FROM FacilityTurnOrder WHERE GameId = ? AND RoundId = ? AND FacilityId = ?;";
-    private static final String READ_FACILITYTURNDELIVER_FOR_FACILITY = "SELECT DeliverAmount, OpenOrderAmount FROM FacilityTurnOrder WHERE GameId = ? AND RoundId = ? AND FacilityId = ?;";
+    private static final String READ_FACILITYTURNORDER_FOR_FACILITY = "SELECT * FROM FacilityTurnOrder WHERE GameId = ? AND RoundId = ? AND FacilityId = ?;";
+    private static final String READ_FACILITYTURNDELIVER_FOR_FACILITY = "SELECT * FROM FacilityTurnOrder WHERE GameId = ? AND RoundId = ? AND FacilityId = ?;";
+    private static final String FACILITY_ID = "FacilityId";
 
     @Inject
     private IDatabaseConnection databaseConnection;
@@ -27,82 +34,124 @@ public class AverageCalculationDAO {
     /**
      * A method to retrieve all the facilities of a certain type.
      *
-     * @param facilityType The facilityType of which the facilities need to be recovered.
+     * @param facilityType The facilityType name from whom the facilities need to be recovered.
+     * @return A list with found facilities from the database which contain the facilityType.
      */
-    public void readAllFacilitiesWithFacilityType(String facilityType) {
+    public List<Integer> readAllFacilitiesWithFacilityType(String facilityType) {
         Connection conn = databaseConnection.connect();
-        if (conn != null) {
-            try (PreparedStatement pstmt = conn.prepareStatement(READ_ALL_FACILITIES_WITH_FACILITYTYPE)) {
-                conn.setAutoCommit(false);
+        List<Integer> facilityIds = new ArrayList<>();
+        try (PreparedStatement pstmt = conn.prepareStatement(READ_ALL_FACILITIES_WITH_FACILITYTYPE)) {
+            conn.setAutoCommit(false);
 
-                DaoConfig.gameIdNotSetCheck(pstmt, 1);
-                pstmt.setString(2, facilityType);
+            DaoConfig.gameIdNotSetCheck(pstmt, 1);
+            pstmt.setString(2, facilityType);
 
-                pstmt.executeUpdate();
-
-                conn.commit();
-            } catch (GameIdNotSetException | SQLException e) {
-                LOGGER.log(Level.SEVERE,e.toString(),e);
-                databaseConnection.rollBackTransaction(conn);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    facilityIds.add(rs.getInt(FACILITY_ID));
+                }
             }
+
+            conn.commit();
+        } catch (GameIdNotSetException | SQLException e) {
+            LOGGER.log(Level.SEVERE, e.toString(), e);
+            databaseConnection.rollBackTransaction(conn);
         }
+        return facilityIds;
     }
 
     /**
-     * A method to retrieve all the FacilityTurns for a certain facility.
+     * A method to retrieve all the FacilityTurns for a certain facility within a specific round.
      *
-     * @param roundId The first part of the identifier for what data needs to be recovered.
+     * @param roundId    The first part of the identifier for what data needs to be recovered.
      * @param facilityId The first part of the identifier for what data needs to be recovered.
+     * @return A FacilityTurn which contains the facilityTurn of a certain facility.
      */
-    public void readFacilityTurnForFacility(int roundId, int facilityId) {
-        executePreparedStatement(roundId, facilityId, READ_FACILITYTURN_FOR_FACILITY);
+    public FacilityTurn readFacilityTurnForFacility(int roundId, int facilityId) {
+        Connection conn = databaseConnection.connect();
+        int backorderAmountStub = 0;
+        FacilityTurn facilityTurn = null;
+        try (PreparedStatement pstmt = conn.prepareStatement(READ_FACILITYTURN_FOR_FACILITY)) {
+            conn.setAutoCommit(false);
+
+            DaoConfig.gameIdNotSetCheck(pstmt, 1);
+            pstmt.setInt(2, roundId);
+            pstmt.setInt(3, facilityId);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                facilityTurn = new FacilityTurn(rs.getInt(FACILITY_ID), rs.getInt("RoundId"), rs.getInt("Stock"), backorderAmountStub, rs.getInt("RemainingBudget"), rs.getBoolean("Bankrupt"));
+            }
+
+            conn.commit();
+        } catch (GameIdNotSetException | SQLException e) {
+            LOGGER.log(Level.SEVERE, e.toString(), e);
+            databaseConnection.rollBackTransaction(conn);
+        }
+        return facilityTurn;
     }
+
 
     /**
      * A method to retrieve all the FacilityTurnOrders for a certain facility.
      *
-     * @param roundId The first part of the identifier for what data needs to be recovered.
+     * @param roundId    The first part of the identifier for what data needs to be recovered.
      * @param facilityId The first part of the identifier for what data needs to be recovered.
+     * @return Returns a list which contains all facilityTurnOrders of a certain facility in a certain round.
      */
-    public void readFacilityTurnOrderForFacility(int roundId, int facilityId) {
-        executePreparedStatement(roundId, facilityId, READ_FACILITYTURNDELIVER_FOR_FACILITY);
+    public List<FacilityTurnOrder> readFacilityTurnOrderForFacility(int roundId, int facilityId) {
+        Connection conn = databaseConnection.connect();
+        List<FacilityTurnOrder> facilityOrderAmounts = new ArrayList<>();
+        try (PreparedStatement pstmt = conn.prepareStatement(READ_FACILITYTURNORDER_FOR_FACILITY)) {
+            conn.setAutoCommit(false);
 
+            DaoConfig.gameIdNotSetCheck(pstmt, 1);
+            pstmt.setInt(2, roundId);
+            pstmt.setInt(3, facilityId);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    facilityOrderAmounts.add(new FacilityTurnOrder(rs.getInt(FACILITY_ID), rs.getInt("FacilityIdOrder"), rs.getInt("OrderAmount")));
+                }
+            }
+
+            conn.commit();
+        } catch (GameIdNotSetException | SQLException e) {
+            LOGGER.log(Level.SEVERE, e.toString(), e);
+            databaseConnection.rollBackTransaction(conn);
+        }
+        return facilityOrderAmounts;
     }
+
 
     /**
      * A method to retrieve all the FacilityTurnDelivers for a certain facility.
      *
-     * @param roundId The first part of the identifier for what data needs to be recovered.
+     * @param roundId    The first part of the identifier for what data needs to be recovered.
      * @param facilityId The first part of the identifier for what data needs to be recovered.
+     * @return Returns a list which contains all FacilityTurnDelivers of a certain facility in a certain round.
      */
-    public void readFacilityTurnDeliverForFacility(int roundId, int facilityId) {
-        executePreparedStatement(roundId, facilityId, READ_FACILITYTURNORDER_FOR_FACILITY);
-    }
-
-    /**
-     * A helper method to set and execute the prepared statement.
-     *
-     * @param roundId The first part of the identifier for what data needs to be recovered.
-     * @param facilityId The first part of the identifier for what data needs to be recovered.
-     * @param query The statement that needs to be executed on the database.
-     */
-    private void executePreparedStatement(int roundId, int facilityId, String query) {
+    public List<FacilityTurnDeliver> readFacilityTurnDeliverForFacility(int roundId, int facilityId) {
         Connection conn = databaseConnection.connect();
-        if (conn != null) {
-            try (PreparedStatement pstmt = conn.prepareStatement(query)) {
-                conn.setAutoCommit(false);
+        List<FacilityTurnDeliver> facilitydeliverAmounts = new ArrayList<>();
+        try (PreparedStatement pstmt = conn.prepareStatement(READ_FACILITYTURNDELIVER_FOR_FACILITY)) {
+            conn.setAutoCommit(false);
 
-                DaoConfig.gameIdNotSetCheck(pstmt, 1);
-                pstmt.setInt(2, roundId);
-                pstmt.setInt(3, facilityId);
+            DaoConfig.gameIdNotSetCheck(pstmt, 1);
+            pstmt.setInt(2, roundId);
+            pstmt.setInt(3, facilityId);
 
-                pstmt.executeUpdate();
-
-                conn.commit();
-            } catch (GameIdNotSetException | SQLException e) {
-                LOGGER.log(Level.SEVERE,e.toString(),e);
-                databaseConnection.rollBackTransaction(conn);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    facilitydeliverAmounts.add(new FacilityTurnDeliver(rs.getInt(FACILITY_ID), rs.getInt("FacilityIdDeliver"), rs.getInt("OpenOrderAmount"), rs.getInt("DeliverAmount")));
+                }
             }
+
+            conn.commit();
+        } catch (GameIdNotSetException | SQLException e) {
+            LOGGER.log(Level.SEVERE, e.toString(), e);
+            databaseConnection.rollBackTransaction(conn);
         }
+        return facilitydeliverAmounts;
     }
+
 }
--- persistence/src/test/java/org/han/ica/asd/c/dao/AverageCalculationDAOIntegrationTest.java
+++ persistence/src/test/java/org/han/ica/asd/c/dao/AverageCalculationDAOIntegrationTest.java
@@ -5,14 +5,25 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.han.ica.asd.c.dbconnection.DBConnectionTest;
 import org.han.ica.asd.c.dbconnection.IDatabaseConnection;
+import org.han.ica.asd.c.model.domain_objects.Facility;
+import org.han.ica.asd.c.model.domain_objects.FacilityType;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 class AverageCalculationDAOIntegrationTest {
     private static final Logger LOGGER = Logger.getLogger(AverageCalculationDAOIntegrationTest.class.getName());
+    private static final FacilityType FACTORYTYPE = new FacilityType("Factory",1,1,1,1,1,1,1);
+    private static final Facility FACTORYONE = new Facility(FACTORYTYPE, 1);
+    private static final Facility FACTORYTWO = new Facility(FACTORYTYPE, 2);
+
+    private AverageCalculationDAO averageCalculationDAO;
+    private FacilityDAO facilityDAO;
+    private FacilityTypeDAO facilityTypeDAO;
 
     @BeforeEach
     void setUp() {
@@ -25,6 +36,25 @@ class AverageCalculationDAOIntegrationTest {
         });
         DBConnectionTest.getInstance().createNewDatabase();
         DaoConfig.setCurrentGameId("BeerGameTest");
+        averageCalculationDAO = injector.getInstance(AverageCalculationDAO.class);
+
+        Injector injectorFaclityDAO = Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                bind(IDatabaseConnection.class).to(DBConnectionTest.class);
+                bind(FacilityTypeDAO.class);
+            }
+        });
+
+        facilityDAO = injectorFaclityDAO.getInstance(FacilityDAO.class);
+
+        Injector injectorFacilityTypeDAO = Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                bind(IDatabaseConnection.class).to(DBConnectionTest.class);
+            }
+        });
+        facilityTypeDAO = injectorFacilityTypeDAO.getInstance(FacilityTypeDAO.class);
     }
 
     @AfterEach
@@ -34,6 +64,15 @@ class AverageCalculationDAOIntegrationTest {
 
     @Test
     void readAllFacilitiesWithFacilityType() {
+        facilityTypeDAO.createFacilityType(FACTORYTYPE);
+        facilityDAO.createFacility(FACTORYONE);
+        facilityDAO.createFacility(FACTORYTWO);
+        Assert.assertEquals(2, facilityDAO.readAllFacilitiesInGame().size());
+
+        List<Integer> facilityIdsDb = averageCalculationDAO.readAllFacilitiesWithFacilityType("Factory");
+        Assert.assertEquals(2, facilityIdsDb.size());
+        Assert.assertEquals(1, facilityIdsDb.get(0).intValue());
+        Assert.assertEquals(2, facilityIdsDb.get(1).intValue());
     }
 
     @Test
