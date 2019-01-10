270c1e8511bbd0a416473ac8a35f834f9383d0f9
JordyVeldhuizen <j.veldhuizen@student.han.nl>
2019-01-10 15:43:45 +0100

    Added Javadoc to IVisualisedPlayedGameData and ReplayComponent

--- replay/src/main/java/org/han/ica/asd/c/replay_data/ReplayComponent.java
+++ replay/src/main/java/org/han/ica/asd/c/replay_data/ReplayComponent.java
@@ -41,14 +41,26 @@ public class ReplayComponent implements IVisualisedPlayedGameData {
         initializeAverageRounds();
     }
 
+    /***
+     * {@inheritDoc}
+     */
+    @Override
     public String getTotalRoundsString() {
         return String.valueOf(totalRounds);
     }
 
+    /***
+     * {@inheritDoc}
+     */
+    @Override
     public String getCurrentRoundString() {
         return String.valueOf(currentRound);
     }
 
+    /***
+     * {@inheritDoc}
+     */
+    @Override
     public boolean incrementCurrentRound() {
         if (currentRound < totalRounds) {
             this.currentRound++;
@@ -57,6 +69,10 @@ public class ReplayComponent implements IVisualisedPlayedGameData {
         return false;
     }
 
+    /***
+     * {@inheritDoc}
+     */
+    @Override
     public boolean decrementCurrentRound() {
         if (currentRound > LOWEST_ROUND_POSSIBLE) {
             this.currentRound--;
@@ -65,16 +81,28 @@ public class ReplayComponent implements IVisualisedPlayedGameData {
         return false;
     }
 
+    /***
+     * {@inheritDoc}
+     */
+    @Override
     public void updateCurrentRound(int round) {
         if (round > totalRounds) currentRound = totalRounds;
         else if (round < LOWEST_ROUND_POSSIBLE) currentRound = LOWEST_ROUND_POSSIBLE;
         else currentRound = round;
     }
 
+    /***
+     * {@inheritDoc}
+     */
+    @Override
     public List<Facility> getAllFacilities() {
         return facilities;
     }
 
+    /***
+     * {@inheritDoc}
+     */
+    @Override
     public ObservableList<XYChart.Series<Double, Double>> getChartData() {
         ObservableList<XYChart.Series<Double, Double>> lineChartData = FXCollections.observableArrayList();
         LineChart.Series<Double, Double> factorySeries = new LineChart.Series<>();
@@ -86,21 +114,45 @@ public class ReplayComponent implements IVisualisedPlayedGameData {
         LineChart.Series<Double, Double> retailerSeries = new LineChart.Series<>();
         retailerSeries.setName(GameValue.RETAILER.getValue()[0]);
         createData(factorySeries, warehouseSeries, wholesalerSeries, retailerSeries);
-        add(lineChartData, factorySeries);
-        add(lineChartData, warehouseSeries);
-        add(lineChartData, wholesalerSeries);
-        add(lineChartData, retailerSeries);
+        addSeriesToChart(lineChartData, factorySeries);
+        addSeriesToChart(lineChartData, warehouseSeries);
+        addSeriesToChart(lineChartData, wholesalerSeries);
+        addSeriesToChart(lineChartData, retailerSeries);
         return lineChartData;
     }
 
+    /***
+     * Creates the data that is returned in the getChartData()
+     * @param factorySeries
+     * A XYChart series that will be filled with Factory data
+     * @param warehouseSeries
+     * A XYChart series that will be filled with Warehouse data
+     * @param wholesalerSeries
+     * A XYChart series that will be filled with WholeSaler data
+     * @param retailerSeries
+     * A XYChart series that will be filled with Retailer data
+     */
     private void createData(XYChart.Series<Double, Double> factorySeries, XYChart.Series<Double, Double> warehouseSeries, XYChart.Series<Double, Double> wholesalerSeries, XYChart.Series<Double, Double> retailerSeries) {
         List<AverageRound> roundsToShow = averageRounds.stream().filter(averageRound ->
                 displayedAverages.contains(averageRound.getFacilityType()) && averageRound.getRoundId() <= currentRound).collect(Collectors.toList());
         roundsToShow.forEach(averageRound ->
-                insertDataIntoSerie(factorySeries, warehouseSeries, wholesalerSeries, retailerSeries, averageRound));
+                insertDataIntoSeries(factorySeries, warehouseSeries, wholesalerSeries, retailerSeries, averageRound));
     }
 
-    private void insertDataIntoSerie(XYChart.Series<Double, Double> factorySeries, XYChart.Series<Double, Double> warehouseSeries, XYChart.Series<Double, Double> wholesalerSeries, XYChart.Series<Double, Double> retailerSeries, AverageRound averageRound) {
+    /***
+     * Inserts the data from a round to the correct series
+     * @param factorySeries
+     * A XYChart series that will be filled with Factory data
+     * @param warehouseSeries
+     * A XYChart series that will be filled with Warehouse data
+     * @param wholesalerSeries
+     * A XYChart series that will be filled with WholeSaler data
+     * @param retailerSeries
+     * A XYChart series that will be filled with Retailer data
+     * @param averageRound
+     * The data that will be inserted into the correct series
+     */
+    private void insertDataIntoSeries(XYChart.Series<Double, Double> factorySeries, XYChart.Series<Double, Double> warehouseSeries, XYChart.Series<Double, Double> wholesalerSeries, XYChart.Series<Double, Double> retailerSeries, AverageRound averageRound) {
         switch (averageRound.getFacilityType()) {
             case FACTORY:
                 factorySeries.getData().add(new XYChart.Data<>((double) averageRound.getRoundId(), getAttribute(averageRound, displayedAttribute)));
@@ -119,32 +171,67 @@ public class ReplayComponent implements IVisualisedPlayedGameData {
         }
     }
 
-    private void add(ObservableList<XYChart.Series<Double, Double>> lineChartData, LineChart.Series<Double, Double> series) {
+    /***
+     * Inserts a series into the Chart if the series is not empty
+     * @param lineChartData
+     * The Chart data
+     * @param series
+     * The series that can be added to the Chart data if the series is not empty
+     */
+    private void addSeriesToChart(ObservableList<XYChart.Series<Double, Double>> lineChartData, LineChart.Series<Double, Double> series) {
         if (!series.getData().isEmpty()) {
             lineChartData.add(series);
         }
     }
 
+    /***
+     * {@inheritDoc}
+     */
+    @Override
     public void removeDisplayedFacility(GameValue facility) {
         displayedAverages.remove(facility);
     }
 
+    /***
+     * {@inheritDoc}
+     */
+    @Override
     public void addDisplayedFacility(GameValue facility) {
         displayedAverages.add(facility);
     }
 
+    /***
+     * {@inheritDoc}
+     */
+    @Override
     public void addDisplayedFacility(Facility facility) {
         displayedFacilities.add(facility);
     }
 
+    /***
+     * {@inheritDoc}
+     */
+    @Override
     public void removeDisplayedFacility(Facility facility) {
         displayedFacilities.remove(facility);
     }
 
+    /***
+     * {@inheritDoc}
+     */
+    @Override
     public void setDisplayedAttribute(GameValue value) {
         this.displayedAttribute = value;
     }
 
+    /***
+     *
+     * @param round
+     * The round from which the value is wanted
+     * @param wantedValue
+     * The GameValue for the wanted attribute
+     * @return The value of the wanted Attribute from the AverageRound
+     */
     public double getAttribute(AverageRound round, GameValue wantedValue) {
         switch (wantedValue) {
             case BUDGET:
@@ -162,6 +249,10 @@ public class ReplayComponent implements IVisualisedPlayedGameData {
         }
     }
 
+    /***
+     * Initializes the list with AverageRounds
+     * Calls the calculateAverageRounds function 4 times, once for each type of Facility available in a game
+     */
     private void initializeAverageRounds() {
         List<Integer> factoryIds = getFacilityIds(GameValue.FACTORY);
         List<Integer> wholesalerIds = getFacilityIds(GameValue.WHOLESALER);
@@ -174,6 +265,13 @@ public class ReplayComponent implements IVisualisedPlayedGameData {
         calculateAverageRounds(warehouseIds, GameValue.REGIONALWAREHOUSE);
     }
 
+    /***
+     * Fills the list of AverageRounds with the calculated round
+     * @param ids
+     * All ids from a specific FacilityType
+     * @param gameValue
+     * The GameValue needed for the AverageRound constructor
+     */
     private void calculateAverageRounds(List<Integer> ids, GameValue gameValue) {
         rounds.forEach(round -> {
             List<FacilityTurn> turns = round.getFacilityTurns().stream().filter(facilityTurn -> ids.contains(facilityTurn.getFacilityId())).collect(Collectors.toList());
@@ -191,51 +289,97 @@ public class ReplayComponent implements IVisualisedPlayedGameData {
         });
     }
 
-    private float calculateAverageOrders(int totalIds, List<FacilityTurnOrder> turns){
+    /***
+     * Calculates the average orders of a round
+     * @param totalIds
+     * The amount of ID's from this FacilityType, used to calculate the average
+     * @param turns
+     * All turns from a round
+     * @return The calculated average from all turns in a round
+     */
+    private float calculateAverageOrders(int totalIds, List<FacilityTurnOrder> turns) {
         float totalOrderAmount = 0;
         for (FacilityTurnOrder turn : turns) {
             totalOrderAmount += turn.getOrderAmount();
         }
 
-        return totalOrderAmount/totalIds;
+        return totalOrderAmount / totalIds;
     }
 
-    private float calculateAverageBudget(int totalIds, List<FacilityTurn> turns){
+    /***
+     * Calculates the average budget of a round
+     * @param totalIds
+     * The amount of ID's from this FacilityType, used to calculate the average
+     * @param turns
+     * All turns from a round
+     * @return The calculated average from all turns in a round
+     */
+    private float calculateAverageBudget(int totalIds, List<FacilityTurn> turns) {
         float totalBudget = 0;
         for (FacilityTurn turn : turns) {
             totalBudget += turn.getRemainingBudget();
         }
 
-        return totalBudget/totalIds;
+        return totalBudget / totalIds;
     }
 
-    private float calculateAverageStock(int totalIds, List<FacilityTurn> turns){
+    /***
+     * Calculates the average stock of a round
+     * @param totalIds
+     * The amount of ID's from this FacilityType, used to calculate the average
+     * @param turns
+     * All turns from a round
+     * @return The calculated average from all turns in a round
+     */
+    private float calculateAverageStock(int totalIds, List<FacilityTurn> turns) {
         float totalStock = 0;
         for (FacilityTurn turn : turns) {
             totalStock += turn.getStock();
         }
 
-        return totalStock/totalIds;
+        return totalStock / totalIds;
     }
 
-    private float calculateAverageBackOrders(int totalIds, List<FacilityTurn> turns){
+    /***
+     * Calculates the average back orders of a round
+     * @param totalIds
+     * The amount of ID's from this FacilityType, used to calculate the average
+     * @param turns
+     * All turns from a round
+     * @return The calculated average from all turns in a round
+     */
+    private float calculateAverageBackOrders(int totalIds, List<FacilityTurn> turns) {
         float totalBackOrders = 0;
         for (FacilityTurn turn : turns) {
             totalBackOrders += turn.getBackorders();
         }
 
-        return totalBackOrders/totalIds;
+        return totalBackOrders / totalIds;
     }
 
-    private float calculateAverageDeliverAmount(int totalIds, List<FacilityTurnDeliver> turns){
+    /***
+     * Calculates the average deliver amount of a round
+     * @param totalIds
+     * The amount of ID's from this FacilityType, used to calculate the average
+     * @param turns
+     * All turns from a round
+     * @return The calculated average from all turns in a round
+     */
+    private float calculateAverageDeliverAmount(int totalIds, List<FacilityTurnDeliver> turns) {
         float totalDeliverAmount = 0;
         for (FacilityTurnDeliver turn : turns) {
             totalDeliverAmount += turn.getDeliverAmount();
         }
 
-        return totalDeliverAmount/totalIds;
+        return totalDeliverAmount / totalIds;
     }
 
+    /***
+     *
+     * @param gameValue
+     * The GameValue of the wanted FacilityType
+     * @return A list of Integers filled with the ID's of 1 FacilityType
+     */
     private List<Integer> getFacilityIds(GameValue gameValue) {
         List<Facility> wantedFacilities = facilities.stream().filter(facility ->
                 facility.getFacilityType().getFacilityName().equals(gameValue.getValue()[0])).collect(Collectors.toList());
