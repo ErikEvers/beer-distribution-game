541ca72e5d2082c848f9afb1b2dc2aa99a49c09e
kaspervdw <K.vandeWetering@student.han.nl>
2019-01-11 09:03:11 +0100

    Added main menu warning confirmation and added internationalization in alert boxes.

--- gui_configure_game/src/main/java/org/han/ica/asd/c/gui_configure_game/assign_agents_to_facilities/AssignAgentsController.java
+++ gui_configure_game/src/main/java/org/han/ica/asd/c/gui_configure_game/assign_agents_to_facilities/AssignAgentsController.java
@@ -4,6 +4,8 @@ import com.google.inject.Inject;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
 import org.han.ica.asd.c.Exceptions.NoProgrammedAgentsFoundException;
@@ -14,6 +16,8 @@ import org.han.ica.asd.c.gameconfiguration.IGameAgentService;
 import org.han.ica.asd.c.model.domain_objects.*;
 
 import javax.inject.Named;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
 public class AssignAgentsController {
 
@@ -36,12 +40,14 @@ public class AssignAgentsController {
 
     private BeerGame beerGame;
     private FacilityRectangle lastClickedFacilityRectangle;
+    private ResourceBundle resourceBundle;
 
     public void initialize() {
+        resourceBundle = ResourceBundle.getBundle("languageResourcesAssignAgents");
         try {
             agentComboBox.setItems(FXCollections.observableArrayList(gameAgentService.getAgentsForUI()));
         } catch (NoProgrammedAgentsFoundException e) {
-            agentComboBox.setPromptText("No local agents found.");
+            agentComboBox.setPromptText(resourceBundle.getString("no_agents_found_warning"));
         }
     }
 
@@ -64,9 +70,8 @@ public class AssignAgentsController {
         if (agentComboBox.getValue() != null) {
             for (int i = 0; i < beerGame.getAgents().size(); i++) {
                 if (agentComboBox.getValue().getProgrammedAgentName().equals(beerGame.getAgents().get(i).getGameAgentName()) && (lastClickedFacilityRectangle.getFacility().getFacilityId() == beerGame.getAgents().get(i).getFacility().getFacilityId())) {
-                    Alert alert = new Alert(Alert.AlertType.INFORMATION, "This agent is already assigned to the selected facility.");
-                    alert.setHeaderText("Warning");
-                    alert.setTitle("Warning");
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION, resourceBundle.getString("agent_assigned_warning"));
+                    alert.setHeaderText(resourceBundle.getString("warning_title"));
                     alert.showAndWait();
                 }
 
@@ -76,9 +81,8 @@ public class AssignAgentsController {
             }
             beerGame.getAgents().add(gameAgentService.createGameAgentFromProgrammedAgent(lastClickedFacilityRectangle.getFacility(), agentComboBox.getValue()));
         } else {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Choose an agent from the available agents.");
-            alert.setHeaderText("Warning");
-            alert.setTitle("Warning");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, resourceBundle.getString("choose_agent_alert"));
+            alert.setHeaderText(resourceBundle.getString("warning_title"));
             alert.showAndWait();
         }
         initTree();
@@ -96,7 +100,13 @@ public class AssignAgentsController {
 
     @FXML
     public void handleBackToMenuButtonClick() {
-        mainMenu.setupScreen();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, resourceBundle.getString("assign_agents_menu_warning"));
+        alert.setTitle(resourceBundle.getString("warning_title"));
+        Optional<ButtonType> clicked = alert.showAndWait();
+
+        if (clicked.get() == ButtonType.OK) {
+            mainMenu.setupScreen();
+        }
     }
 
     @FXML
