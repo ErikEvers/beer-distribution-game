412d84884c9fbc3c75c1c0569cff4bcfc7a1b834
Erik Evers <E.Evers1@student.han.nl>
2019-01-14 11:56:42 +0100

    Fixed missing dependencies for injection in tests

---
+++ gameleader/src/test/java/org/han/ica/asd/c/gameleader/stubs/BusinessRulesStub.java
@@ -0,0 +1,21 @@
+package org.han.ica.asd.c.gameleader.stubs;
+
+import org.han.ica.asd.c.interfaces.businessrule.IBusinessRules;
+import org.han.ica.asd.c.model.domain_objects.Round;
+import org.han.ica.asd.c.model.interface_models.ActionModel;
+import org.han.ica.asd.c.model.interface_models.UserInputBusinessRule;
+
+import java.util.List;
+
+public class BusinessRulesStub implements IBusinessRules {
+
+    @Override
+    public List<UserInputBusinessRule> programAgent(String agentName, String businessRules) {
+        return null;
+    }
+
+    @Override
+    public ActionModel evaluateBusinessRule(String businessRule, Round round, int facilityId) {
+        return null;
+    }
+}
