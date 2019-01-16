3558546302edc08df75294028dc7090a5d03d160
Oscar Katsma <OT.Katsma@student.han.nl>
2019-01-16 12:07:12 +0100

    Revert messageDirector

--- communication/src/main/java/org/han/ica/asd/c/MessageDirector.java
+++ communication/src/main/java/org/han/ica/asd/c/MessageDirector.java
@@ -11,9 +11,11 @@ import javax.inject.Inject;
 import java.io.InvalidObjectException;
 
 public class MessageDirector implements IServerObserver {
+
     @Inject
     private GameMessageReceiver gameMessageReceiver;
 
+    @Inject
     private FaultDetectionMessageReceiver faultDetectionMessageReceiver;
 
     public MessageDirector() {
@@ -24,23 +26,22 @@ public class MessageDirector implements IServerObserver {
      * This method receives an object from a socket connection and delegates the object to GameMessageReceiver.
      * If any FaultDetectionMessages expect a response, return that response, don't return null.
      * If any object is returned here, that object should be expected in the client that sent the original message.
-     *
      * @param receivedObject
      * @return Object
      */
     @Override
     public Object serverObjectReceived(Object receivedObject, String senderIp) {
-            if (receivedObject instanceof GameMessage) {
-                GameMessage gameMessage = (GameMessage) receivedObject;
-                return gameMessageReceiver.gameMessageReceived(gameMessage, senderIp);
 
-            } else if (receivedObject instanceof FaultDetectionMessage && faultDetectionMessageReceiver != null) {
-                FaultDetectionMessage faultDetectionMessage = (FaultDetectionMessage) receivedObject;
-                return faultDetectionMessageReceiver.receiveMessage(faultDetectionMessage, senderIp);
-            } else {
-                return new ResponseMessage(false, new InvalidObjectException("Invalid object"));
-            }
+        if (receivedObject instanceof GameMessage) {
+            GameMessage gameMessage = (GameMessage) receivedObject;
+            return gameMessageReceiver.gameMessageReceived(gameMessage, senderIp);
 
+        } else if (receivedObject instanceof FaultDetectionMessage) {
+            FaultDetectionMessage faultDetectionMessage = (FaultDetectionMessage) receivedObject;
+            return faultDetectionMessageReceiver.receiveMessage(faultDetectionMessage, senderIp);
+        } else {
+            return new ResponseMessage(false, new InvalidObjectException("Invalid object"));
+        }
     }
 
     @Override
