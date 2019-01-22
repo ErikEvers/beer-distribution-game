063fec0ee80c76fb2ced7d4267eef8c0432ae47e
Yarno Boelens <yarno@print.com>
2019-01-22 17:54:28 +0100

    Added forgotten file

---
+++ fxml_helper/src/main/java/org/han/ica/asd/c/fxml_helper/NumberInputFormatter.java
@@ -0,0 +1,26 @@
+package org.han.ica.asd.c.fxml_helper;
+
+import javafx.scene.control.TextFormatter;
+
+import java.util.function.UnaryOperator;
+
+public class NumberInputFormatter {
+	/**
+	 * Retrieves a formatter that can be applied to text fields to ensure numbers only
+	 * @return
+	 * The text formatter
+	 */
+	static public UnaryOperator<TextFormatter.Change> getChangeUnaryOperator() {
+		return change -> {
+			String newText = change.getControlNewText();
+			if (newText.matches("[0-9]+")){
+				return change;
+			}
+			if (newText.length() < 1) {
+				change.setText("0");
+				return change;
+			}
+			return null;
+		};
+	}
+}
