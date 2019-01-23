b796c06a90d03c1c54965c10c1cb71398fa0320a
RickGZ <rickgz66@gmail.com>
2019-01-23 16:11:34 +0100

    added 10 character limit to username field
    to prevent overlap when viewing facilities

--- gui_main_menu/src/main/java/org/han/ica/asd/c/gui_main_menu/MainMenuController.java
+++ gui_main_menu/src/main/java/org/han/ica/asd/c/gui_main_menu/MainMenuController.java
@@ -64,6 +64,20 @@ public class MainMenuController {
         connector.start();
 
         fillIpComboBox();
+
+        addTextLimiter(usernameField, 10);
+    }
+
+    private void addTextLimiter(final TextField tf, final int maxLength) {
+        tf.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
+                if (tf.getText().length() > maxLength) {
+                    String s = tf.getText().substring(0, maxLength);
+                    tf.setText(s);
+                }
+            }
+        });
     }
 
     private void fillIpComboBox(){
